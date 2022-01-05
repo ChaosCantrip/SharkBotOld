ae38944abe41897325563819edd12ba2b71e06e3
ChaosCantrip <james.donald.02@gmail.com>
2022-01-05 03:36:56 +0000

    Altered main to work with cogs

--- main.py
+++ main.py
@@ -19,75 +19,6 @@ if secret.testBot:
 else:
     import ids
 
-
-async def report_error(message, error):
-    chaos = await bot.fetch_user(ids.users["Chaos"])
-    report = f"ERROR REPORT \nRaised by:\n{message.author.name} : {message.author.id}\n"
-    if message.guild != None:
-        report = report + f"in {message.guild.name}, {message.channel.name} : {message.guild.id}, {message.channel.id}"
-    else:
-        report = report + "in DMs"
-    report = report + f"\nMessage ID: {message.id} \nMessage Text: {message.content} \n \nError:\n"
-    
-    for note in sys.exc_info():
-        report = report + str(note) + "\n"
-    await chaos.send("```" + report + "```")
-
-    
-    
-@bot.event
-async def on_ready():
-    print(f"Sharkbot ready on {bot.user} : {bot.user.id}")
-    chaos = await bot.fetch_user(ids.users["Chaos"])    
-    
-    await chaos.send("SharkBot is up and running!")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="nom nom nom!"))
-    await count.check_list(bot)
-    
-    r = open("reboot.txt", "r")
-    replyTxt = r.read()
-    replyFlag, replyID = replyTxt.split()
-    r.close()
-    
-    if replyFlag == "True":
-        replyChannel = await bot.fetch_channel(int(replyID))
-        await replyChannel.send("I'm back!")
-        w = open("reboot.txt", "w")
-        w.write(f"False {replyID}")
-        w.close()
-    
-    print("")
-    print("The bot is currently in these servers:")
-
-    for guild in bot.guilds:
-        print(f"{guild.name} : {guild.id}")
-
-        
-        
-@bot.command()
-async def verify(message):
-    await count.verify_count(bot, message)
-
-
-
-@bot.command()
-async def tally(message):
-    await count.tally(bot, message)
-
-
-    
-@bot.command()
-async def tallychannel(message, arg):
-    if message.author.id in ids.mods:
-        await count.tally_channel(bot, message, int(arg)) 
-
-
-        
-@bot.command()
-async def timeline(message):
-    await count.timeline(bot, message)
-
-
     
 @bot.command()
 async def reboot(message):
@@ -106,40 +37,25 @@ async def reboot(message):
 
 
 @bot.command()
-async def ping(message):
-    await message.channel.send("Pong!")
+async def load(message, extension):
+    bot.load_extension(f"cogs.{extension}")
+    print(f"{extension} loaded.")
 
 
     
-@bot.event
-async def on_message(message):
-    try:
-        await process_message(message)
-    except Exception as e:
-        await report_error(message, e)
-
-        
-        
-@bot.event
-async def on_message_edit(oldMessage, newMessage):
-    try:
-        await count.verify_message_edit(bot, newMessage)
-    except Exception as e:
-        await report_error(newMessage, e)
-
-async def process_message(message):
-        if message.author == bot.user:
-            return
-
-        if message.author.id in ids.blacklist:
-            return
-
-        if message.channel.id == ids.channels["Count"]:
-            await count.process_message(bot, message)
+@bot.command()
+async def unload(message, extension):
+    bot.unload_extension(f"cogs.{extension}")
+    print(f"{extension} unloaded.")
 
 
-        await bot.process_commands(message)
-
-
+    
+@bot.command()
+async def reload(message, extension):
+    bot.unload_extension(f"cogs.{extension}")
+    print(f"{extension} unloaded.")
+    bot.load_extension(f"cogs.{extension}")
+    print(f"{extension} loaded.")
+    print(f"{extension} reloaded.")
         
 bot.run(secret.token)
