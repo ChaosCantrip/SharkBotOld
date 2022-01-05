4781729eacdfc1e958e43df87617a461cf7016ea
ChaosCantrip <james.donald.02@gmail.com>
2022-01-05 18:10:39 +0000

    Added Economy cog

---
+++ cogs/economy.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+import secret
+if secret.testBot:
+    import testids as ids
+else:
+    import ids
+
+
+
+class Economy(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+
+def setup(bot):
+    bot.add_cog(Economy(bot))
