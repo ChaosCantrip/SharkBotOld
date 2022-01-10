af8004ddb1ac8a5d5302b1c086d1dd040513948f
ChaosCantrip <james.donald.02@gmail.com>
2022-01-10 08:06:48 +0000

    Revert "Revert "Moved error handling to Errors Cog""
    This reverts commit 5aa224edf8470a04af73c021dad17eec535b6268.

--- main.py
+++ main.py
@@ -116,32 +116,6 @@ async def pull(ctx):
 
 
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send("Sorry, I don't know that command!")
-        return
-    if isinstance(error, commands.CheckAnyFailure):
-        await ctx.send("Sorry, you can't do that!")
-        return
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("I think you're missing an argument there!")
-        return
-    if isinstance(error, commands.ChannelNotFound):
-        await ctx.send("Please enter a valid channel!")
-        return
-    if isinstance(error, commands.errors.BadArgument):
-        await ctx.send("Please enter a valid number!")
-        return
-    if isinstance(error, commands.ExtensionNotLoaded):
-        await ctx.send("Extension not found!")
-        return
-
-    raise error
-
-
-
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
