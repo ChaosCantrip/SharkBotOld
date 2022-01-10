5fcd11f77e42ff4b1c6e37cdccbc379ac86e8646
ChaosCantrip <james.donald.02@gmail.com>
2022-01-10 20:51:39 +0000

    Added Lootboxes.py

---
+++ cogs/lootboxes.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import tasks, commands
+
+import secret
+if secret.testBot:
+	import testids as ids
+else:
+	import ids
+
+	
+	
+class Lootboxes(commands.Cog):
+	
+	def __init__(self, bot):
+		self.bot = bot
+		
+		
+		
+def setup(bot):
+	bot.add_cog(Lootboxes(bot))
+	print("Lootboxes Cog loaded")
+
+def teardown(bot):
+	print("Lootboxes Cog unloaded")
+	bot.remove_cog(Lootboxes(bot))
