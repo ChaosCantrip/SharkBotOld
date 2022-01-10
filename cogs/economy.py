c56703bb809e3afd6ceb7f392dd3042ac09634d2
ChaosCantrip <james.donald.02@gmail.com>
2022-01-10 08:46:59 +0000

    Fixed unresolved import discord.ext.tasks

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
 from asyncio import TimeoutError
 
 import secret
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
 
 import sys
 import os
