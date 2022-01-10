6798ed92da093335b2f082656a42d2cf3ae302c5
ChaosCantrip <james.donald.02@gmail.com>
2022-01-10 08:04:56 +0000

    Revert "Fixed whitespace issues"
    This reverts commit 411ede22352baeb18e501f8dda627df66598f619.

--- errors.py
+++ errors.py
@@ -11,13 +11,12 @@ else:
 	
 class Errors(commands.Cog):
 	
-    def __init__(self, bot):
-        self.bot = bot
-
-
-
+	def __init__(self, bot):
+		self.bot = bot
+		
+		
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
+    async def on_command_error(ctx, error):
         if isinstance(error, commands.CommandNotFound):
             await ctx.send("Sorry, I don't know that command!")
             return
@@ -36,11 +35,9 @@ class Errors(commands.Cog):
         if isinstance(error, commands.ExtensionNotLoaded):
             await ctx.send("Extension not found!")
             return
-        
-        raise error
 
-
-
+    raise error
+		
 def setup(bot):
 	bot.add_cog(Errors(bot))
 	print("Errors Cog loaded")
