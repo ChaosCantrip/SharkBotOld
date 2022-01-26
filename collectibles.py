7ac227b39683161cdd3c74dabf1b05f0d856a326
ChaosCantrip <james.donald.02@gmail.com>
2022-01-26 18:32:07 +0000

    Setup definitions for rarities and items

--- collectibles.py
+++ collectibles.py
@@ -1,4 +1,56 @@
-from collectibles import common, uncommon, rare, legendary, exotic
+class Item():
+	
+	def __init__(self, itemData):
+		self.id, self.name, self.description = itemData
+		self.price = int(itemData[-1])
+		self.rarity = Rarities.ref[self.id[0]]
 
-table = common.table + uncommon.table + rare.table + legendary.table + exotic.table
-collection = {**common.collection, **uncommon.collection, **rare.collection, **legendary.collection, **exotic.collection}
+
+
+class Collection():
+	
+	def __init__(self, rarity, file):
+		self.name = rarity.name
+		self.colour = rarity.colour
+		self.rarity = rarity
+		
+		r = open(file, "r")
+		fileData = r.read()
+		r.close()
+
+		self.collection = {}
+		for line in fileData.split("\n"):
+			itemData = line.split(";")
+			self.collection[itemData[0]] = Item(itemData)
+
+
+
+class Rarity():
+
+	def __init__(self, name, colour):
+		self.name = name
+		self.colour = colour
+
+
+
+class Rarities():
+	
+	def __init__(self):
+		self.common = Rarity("Common", "#808080")
+		self.uncommon = Rarity("Uncommon", "#90ee90")
+		self.rare = Rarity("Rare", "#B9CFF0")
+		self.legendary = Rarity("Legendary", "#CC8899")
+		self.exotic = Rarity("Exotic", "#FFD300")
+
+
+
+class Collections():
+
+	def __init__(self):
+		self.common = Collection(Rarities.common, "common.txt")
+		self.uncommon = Collection(Rarities.uncommon, "uncommon.txt")
+		self.rare = Collection(Rarities.rare, "rare.txt")
+		self.legendary = Collection(Rarities.legendary, "legendary.txt")
+		self.exotic = Collection(Rarities.exotic, "exotic.txt")
+
+
