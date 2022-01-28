193732dd245439c6404c3df0c22faeee87ac136f
ChaosCantrip <james.donald.02@gmail.com>
2022-01-28 02:33:38 +0000

    Added icon to opening

--- cogs/collectibles.py
+++ cogs/collectibles.py
@@ -278,7 +278,7 @@ class Collectibles(commands.Cog):
 				
 				embed = discord.Embed()
 				embed.title = f"{box.name} opened!"
-				embed.description = f"You got *{item.name}*!"
+				embed.description = f"You got {item.rarity.icon} *{item.name}*!"
 				embed.color = item.rarity.colour
 
 				await ctx.send(embed=embed)
