c391959ca7ce293d214dbefb4431e97250f5e516
ChaosCantrip <james.donald.02@gmail.com>
2022-01-31 02:44:37 +0000

    Fixed $hourly

--- cogs/collectibles.py
+++ cogs/collectibles.py
@@ -373,8 +373,8 @@ class Collectibles(commands.Cog):
 		timeCheck, timeDifference = check_cooldown(ctx.author.id, 0, 60*60)
 		if timeCheck == True:
 			lootbox = find_item_by_id("LOOT0")
-			collections[ctx.author.id].append(lootbox)
-			await ctx.send("Success! You claimed a f{lootbox.name}!")
+			inventories[ctx.author.id].append(lootbox)
+			await ctx.send(f"Success! You claimed a {lootbox.name}!")
 		else:
 			await ctx.send(f"Slow down there! You still have {convert_td_to_string(60*60 - timeDifference)} left before you can do that.")
 
