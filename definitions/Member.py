9bc9b171fe7f428d147ebc7ac95941d33b816178
ChaosCantrip <james.donald.02@gmail.com>
2022-03-25 03:26:42 +0000

    Updated Member.get() to use BlankMember

--- definitions/Member.py
+++ definitions/Member.py
@@ -11,7 +11,8 @@ class BlankMember(Member):
 
 def get(member_id):
     try:
-        return Member(member_id)
+        member = Member(member_id)
     except MemberNotFoundError:
-        create(member_id)
-        return Member(member_id)
+        member = BlankMember(member_id)
+        member.write_data()
+    return member
