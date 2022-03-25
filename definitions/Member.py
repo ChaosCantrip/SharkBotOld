b2c7e3d4601238615e3c9abc4d63c1d3c504b2d0
ChaosCantrip <james.donald.02@gmail.com>
2022-03-25 03:21:17 +0000

    Added Member.get()

--- definitions/Member.py
+++ definitions/Member.py
@@ -1,2 +1,9 @@
 class Member():
     pass
+
+def get(member_id):
+    try:
+        return Member(member_id)
+    except MemberNotFoundError:
+        create(member_id)
+        return Member(member_id)
