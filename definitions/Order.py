3e478277c99e1b54b6ecda7428d262ce28a7ddb5
ChaosCantrip <james.donald.02@gmail.com>
2022-03-26 06:46:28 +0000

    Added Order.OrderItem class

--- definitions/Order.py
+++ definitions/Order.py
@@ -1,3 +1,11 @@
+class OrderItem():
+    
+    def __init__(self, data):
+        self.index = data["id"]
+        self.product_id = data["product_id"]
+        self.product_name = data["name"]
+        self.quantity = data["quantity"]
+
 class Order():
 
     def __init__(self, data):
