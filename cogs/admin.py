c0c85b924a57c1767bee52285a1138ddcaf8cbdc
ChaosCantrip <james.donald.02@gmail.com>
2022-04-24 00:39:31 +0100

    Populated Admin Cog

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,1 +1,25 @@
+import discord
+from discord.ext import tasks, commands
 
+import secret
+if secret.testBot:
+	import testids as ids
+else:
+	import ids
+
+	
+	
+class Admin(commands.Cog):
+	
+	def __init__(self, bot):
+		self.bot = bot
+		
+		
+		
+def setup(bot):
+	bot.add_cog(Admin(bot))
+	print("Admin Cog loaded")
+
+def teardown(bot):
+	print("Admin Cog unloaded")
+	bot.remove_cog(Admin(bot))
