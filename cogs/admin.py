67beb8ede5d7a1f5a55a71d3ddbcdb032cdbe279
ChaosCantrip <james.donald.02@gmail.com>
2022-04-24 00:39:58 +0100

    Moved $migrate and $summon from Core Cog to Admin Cog

--- cogs/admin.py
+++ cogs/admin.py
@@ -13,6 +13,30 @@ class Admin(commands.Cog):
 	
 	def __init__(self, bot):
 		self.bot = bot
+
+    @commands.command()
+    @commands.has_role(ids.roles["Mod"])
+    async def migrate(self, ctx, *, newChannel: discord.VoiceChannel):
+        currentChannel = ctx.author.voice.channel
+        if currentChannel == None:
+            await ctx.send("You're not in a voice channel!")
+            return
+        members = list(currentChannel.members)
+        for member in members:
+            await member.move_to(newChannel)
+        await ctx.send(f"Moved *{len(members)}* members from {currentChannel.mention} to {newChannel.mention}.")
+
+    @commands.command()
+    @commands.has_role(ids.roles["Mod"])
+    async def summon(self, ctx, *, targetChannel: discord.VoiceChannel):
+        currentChannel = ctx.author.voice.channel
+        if currentChannel == None:
+            await ctx.send("You're not in a voice channel!")
+            return
+        members = list(targetChannel.members)
+        for member in members:
+            await member.move_to(currentChannel)
+        await ctx.send(f"Moved *{len(members)}* members from {targetChannel.mention} to {currentChannel.mention}.")
 		
 		
 		
--- cogs/core.py
+++ cogs/core.py
@@ -36,30 +36,6 @@ class Core(commands.Cog):
     async def send(self, ctx, channel: discord.TextChannel, *, text):
         await channel.send(text)
 
-    @commands.command()
-    @commands.has_role(ids.roles["Mod"])
-    async def migrate(self, ctx, *, newChannel: discord.VoiceChannel):
-        currentChannel = ctx.author.voice.channel
-        if currentChannel == None:
-            await ctx.send("You're not in a voice channel!")
-            return
-        members = list(currentChannel.members)
-        for member in members:
-            await member.move_to(newChannel)
-        await ctx.send(f"Moved *{len(members)}* members from {currentChannel.mention} to {newChannel.mention}.")
-
-    @commands.command()
-    @commands.has_role(ids.roles["Mod"])
-    async def summon(self, ctx, *, targetChannel: discord.VoiceChannel):
-        currentChannel = ctx.author.voice.channel
-        if currentChannel == None:
-            await ctx.send("You're not in a voice channel!")
-            return
-        members = list(targetChannel.members)
-        for member in members:
-            await member.move_to(currentChannel)
-        await ctx.send(f"Moved *{len(members)}* members from {targetChannel.mention} to {currentChannel.mention}.")
-
 
 
 def setup(bot):
