741885bba25aa4c32f7d69eeabb8749fb1b00e13
ChaosCantrip <james.donald.02@gmail.com>
2022-04-24 00:09:34 +0100

    Removed $countdown

--- cogs/core.py
+++ cogs/core.py
@@ -109,56 +109,6 @@ class Core(commands.Cog):
             await member.move_to(currentChannel)
         await ctx.send(f"Moved *{len(members)}* members from {targetChannel.mention} to {currentChannel.mention}.")
 
-    @commands.command()
-    async def countdown(self, ctx):
-        now = datetime.datetime.now()
-        raidtime = datetime.datetime(2022, 3, 5, 18)
-        timediff = raidtime - now
-        td = timediff.total_seconds()
-
-        seconds = int(td)
-        days, seconds = seconds // (24*60*60), seconds % (24*60*60)
-        hours, seconds = seconds // (60*60), seconds % (60*60)
-        minutes, seconds = seconds // 60, seconds % 60
-
-        outputString = ""
-        if days != 0:
-            if days == 1:
-                outputString += f"{days} day, "
-            else:
-                outputString += f"{days} days, "
-        if hours != 0:
-            if hours == 1:
-                outputString += f"{hours} hour, "
-            else:
-                outputString += f"{hours} hours, "
-        if minutes != 0:
-            if minutes == 1:
-                outputString += f"{minutes} minute, "
-            else:
-                outputString += f"{minutes} minutes, "
-        if outputString == "":
-            if seconds == 1:
-                outputString += f"{seconds} second "
-            else:
-                outputString += f"{seconds} seconds "
-        else:
-            outputString = outputString[:-2] + f" and {seconds} "
-            if seconds == 1:
-                outputString += f"second "
-            else:
-                outputString += f"seconds "
-        
-        embed = discord.Embed()
-        embed.title = "Vow of The Disciple Raid Countdown"
-        embed.description = f"{outputString} until the raid race begins!"
-        embed.color = 0x758B72
-        try:
-            embed.set_thumbnail(url="https://i.pinimg.com/originals/87/93/bc/8793bc836dabd48ae51381b7c43e74dd.jpg")
-        except:
-            pass
-        await ctx.send(embed=embed)
-
 
 
 
