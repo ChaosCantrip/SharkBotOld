0762d6528cc36f5dc47f26d182aff9c03ef82aef
ChaosCantrip <james.donald.02@gmail.com>
2022-05-02 20:59:45 +0100

    Added comments to decorator snippet

--- snippets.py
+++ snippets.py
@@ -1,5 +1,11 @@
 
+
+##Decorator snippet
 def decorator(func):
     def wrapper(*args, **kwargs):
+        ##Do stuff before
+
         func(*args, **kwargs)
+
+        ##Do stuff after
     return wrapper
