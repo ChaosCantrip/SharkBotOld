972a4f6890396f531b332f4c4189253df971f81d
ChaosCantrip <james.donald.02@gmail.com>
2022-06-15 06:19:10 +0100

    Fixed current Collection name Capitalisation and spaces

--- definitions/Collection.py
+++ definitions/Collection.py
@@ -21,18 +21,18 @@ class Collection():
             self.icon = discord.utils.get(server.emojis, name=self.iconName)
         return self.icon
 
-common = Collection("C", "common", "common_item", discord.Color.light_grey())
-uncommon = Collection("U", "uncommon", "uncommon_item", discord.Color.green())
-rare = Collection("R", "rare", "rare_item", 0x6fa8dc)
-legendary = Collection("L", "legendary", "legendary_item", discord.Color.dark_purple())
-exotic = Collection("E", "exotic", "exotic_item", discord.Color.gold())
-mythic = Collection("M", "mythic", "mythic_item", discord.Color.red())
+common = Collection("C", "Common", "common_item", discord.Color.light_grey())
+uncommon = Collection("U", "Uncommon", "uncommon_item", discord.Color.green())
+rare = Collection("R", "Rare", "rare_item", 0x6fa8dc)
+legendary = Collection("L", "Legendary", "legendary_item", discord.Color.dark_purple())
+exotic = Collection("E", "Exotic", "exotic_item", discord.Color.gold())
+mythic = Collection("M", "Mythic", "mythic_item", discord.Color.red())
 
-lootboxes = Collection("LOOT", "lootboxes", "lootboxes_item", discord.Color.orange())
+lootboxes = Collection("LOOT", "Lootboxes", "lootboxes_item", discord.Color.orange())
 
-valentines = Collection("LOVE", "valentines", "valentines_item", 0xfb00ff)
-witch_queen = Collection("WQ", "witch queen", "witch queen_item", 0x758B72)
-easter = Collection("EA", "easter", "easter_item", 0xF8E27F)
+valentines = Collection("LOVE", "Valentines", "valentines_item", 0xfb00ff)
+witch_queen = Collection("WQ", "Witch Queen", "witch_queen_item", 0x758B72)
+easter = Collection("EA", "Easter", "easter_item", 0xF8E27F)
 
 collections = [
     common,
