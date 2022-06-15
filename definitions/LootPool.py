a7c7c53ebe5b92b5af75cbf0ecf316334fbf9345
ChaosCantrip <james.donald.02@gmail.com>
2022-06-15 07:17:05 +0100

    Added LootPool.roll()

--- definitions/LootPool.py
+++ definitions/LootPool.py
@@ -11,3 +11,10 @@ class LootPool():
             collection = Collection.get(splitCode[0])
             chance = int(splitCode[1])
             lootPool[collection] = chance
+
+    def roll(self):
+        collections = list(lootPool.keys())
+        weights = list(lootPool.values())
+        rolledCollection = random.choices(collections, weights=weights, k=1)[0]
+        rolledItem = random.choice(rolledCollection.items)
+        return rolledItem
