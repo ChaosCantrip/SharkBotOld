004c66161c63936674c76df08b80df2e183c3ec8
ChaosCantrip <james.donald.02@gmail.com>
2022-06-15 07:16:56 +0100

    Added LootPool constructor

--- definitions/LootPool.py
+++ definitions/LootPool.py
@@ -1,3 +1,13 @@
+from definitions import Collection.get
+import random
 
 class LootPool():
-    pass
+    
+    def __init__(self, lootPoolCode):
+        lootPoolCodes = lootPoolCode.split(";")
+        lootPool = {}
+        for code in lootPoolCodes:
+            splitCode = code.split(":")
+            collection = Collection.get(splitCode[0])
+            chance = int(splitCode[1])
+            lootPool[collection] = chance
