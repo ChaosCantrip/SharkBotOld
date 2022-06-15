2ae2fcb9561ec869f9af82cc345c067296e0961c
ChaosCantrip <james.donald.02@gmail.com>
2022-06-15 07:21:18 +0100

    Fixed LootPool.lootPool definition

--- definitions/LootPool.py
+++ definitions/LootPool.py
@@ -5,16 +5,16 @@ class LootPool():
     
     def __init__(self, lootPoolCode):
         lootPoolCodes = lootPoolCode.split(";")
-        lootPool = {}
+        self.lootPool = {}
         for code in lootPoolCodes:
             splitCode = code.split(":")
             collection = Collection.get(splitCode[0])
             chance = int(splitCode[1])
-            lootPool[collection] = chance
+            self.lootPool[collection] = chance
 
     def roll(self):
-        collections = list(lootPool.keys())
-        weights = list(lootPool.values())
+        collections = list(self.lootPool.keys())
+        weights = list(self.lootPool.values())
         rolledCollection = random.choices(collections, weights=weights, k=1)[0]
         rolledItem = random.choice(rolledCollection.items)
         return rolledItem
