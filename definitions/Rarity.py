93c46234dd26b0e34542431040cfcad6dd6e51d1
ChaosCantrip <james.donald.02@gmail.com>
2022-06-15 06:31:40 +0100

    Added 10 current Rarities

--- definitions/Rarity.py
+++ definitions/Rarity.py
@@ -3,3 +3,16 @@ class Rarity():
     def __init__(self, name, value):
         self.name = name
         self.value = value
+
+common = Rarity("Common", 5)
+uncommon = Rarity("Uncommon", 10)
+rare = Rarity("Rare", 20)
+legendary = Rarity("Legendary", 50)
+exotic = Rarity("Exotic", 150)
+mythic = Rarity("Mythic", 500)
+
+lootboxes = Rarity("Lootboxes", 100)
+
+valentines = Rarity("Valentines", 10)
+witch_queen = Rarity("Witch Queen", 10)
+easter = Rarity("Easter", 10)
