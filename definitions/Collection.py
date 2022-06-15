d854f669d3d9c1be3933e75786376a65ea2afdd3
ChaosCantrip <james.donald.02@gmail.com>
2022-06-15 06:06:57 +0100

    Added Collection.get_icon()

--- definitions/Collection.py
+++ definitions/Collection.py
@@ -1,7 +1,22 @@
+import discord.utils.get
+
+import secret
+if secret.testBot:
+    import testids as ids
+else:
+    import ids
+
 class Collection():
     
     def __init__(self, id, name, iconName, colour):
         self.id = id
         self.name = name
         self.iconName = iconName
+        self.icon = None
         self.colour = colour
+
+    async def get_icon(self, bot):
+        if self.icon == None:
+            server = await bot.fetch_guild(ids.server)
+            self.icon = discord.utils.get(server.emojis, name=self.iconName)
+        return self.icon
