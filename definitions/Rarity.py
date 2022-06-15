38d6439bf2e35000c7fde1351cbdf673e1e0017d
ChaosCantrip <james.donald.02@gmail.com>
2022-06-15 07:54:39 +0100

    Added Rarity.get_icon()

--- definitions/Rarity.py
+++ definitions/Rarity.py
@@ -1,23 +1,30 @@
 from definitions import SharkErrors
+import discord
 
 class Rarity():
     
-    def __init__(self, name, value):
+    def __init__(self, name, value, iconName):
         self.name = name
         self.value = value
+        self.iconName = iconName
 
-common = Rarity("Common", 5)
-uncommon = Rarity("Uncommon", 10)
-rare = Rarity("Rare", 20)
-legendary = Rarity("Legendary", 50)
-exotic = Rarity("Exotic", 150)
-mythic = Rarity("Mythic", 500)
+    def get_icon(self, server):
+        if self.icon == None:
+            self.icon = discord.utils.get(server.emojis, name=self.iconName)
+        return self.icon
 
-lootboxes = Rarity("Lootboxes", 100)
+common = Rarity("Common", 5, "common_item")
+uncommon = Rarity("Uncommon", 10, "uncommon_item")
+rare = Rarity("Rare", 20, "rare_item")
+legendary = Rarity("Legendary", 50, "legendary_item")
+exotic = Rarity("Exotic", 150, "exotic_item")
+mythic = Rarity("Mythic", 500, "mythic_item")
 
-valentines = Rarity("Valentines", 10)
-witch_queen = Rarity("Witch Queen", 10)
-easter = Rarity("Easter", 10)
+lootboxes = Rarity("Lootboxes", 100, "lootboxes_item")
+
+valentines = Rarity("Valentines", 10, "valentines_item")
+witch_queen = Rarity("Witch Queen", 10, "witch_queen_item")
+easter = Rarity("Easter", 10, "easter_item")
 
 rarities = [common, uncommon, rare, legendary, exotic, lootboxes, mythic, valentines, witch_queen, easter]
 
