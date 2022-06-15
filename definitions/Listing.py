fe5c4223ffad5005606ee7430bea30d2a4f10bc0
ChaosCantrip <james.donald.02@gmail.com>
2022-06-15 06:41:31 +0100

    Altered Listing.py to import shop.txt

--- definitions/Listing.py
+++ definitions/Listing.py
@@ -1,3 +1,16 @@
-
 class Listing():
-    pass
+
+    def __init__(self, listingDataString):
+        listingData = listingDataString.split(":")
+        self.item = search_for_item(listingData[0])
+        self.price = int(listingData[1])
+
+listings = []
+
+with open("data/collectibles/shop.txt", "r") as infile:
+    fileData = infile.read()
+
+for line in fileData.split("\n"):
+    if line == "":
+        continue
+    listings.append(Listing(line))
