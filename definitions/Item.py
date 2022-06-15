19a11419c82d5beb7c26f6fcc02a71c34fd85535
ChaosCantrip <james.donald.02@gmail.com>
2022-06-15 06:47:21 +0100

    Added Item.search()

--- definitions/Item.py
+++ definitions/Item.py
@@ -19,3 +19,12 @@ def get(search: str):
             if search == item.id:
                 return item
     raise SharkErrors.ItemNotFoundError(search)
+
+def search(search: str):
+    search = search.upper()
+    for collection in Collection.collections:
+        for item in collection.items:
+            if search == item.id or search == item.name.upper():
+                return item
+    raise SharkErrors.ItemNotFoundError(search)
+
