f53926aed12480be84a4f23f9b1b8285b9fb28d2
ChaosCantrip <james.donald.02@gmail.com>
2022-07-08 16:06:03 +0100

    Added Summer Rarity

--- definitions/Rarity.py
+++ definitions/Rarity.py
@@ -26,8 +26,9 @@ lootboxes = Rarity("Lootboxes", 100, "lootboxes_item")
 valentines = Rarity("Valentines", 10, "valentines_item")
 witch_queen = Rarity("Witch Queen", 10, "witch_queen_item")
 easter = Rarity("Easter", 10, "easter_item")
+summer = Rarity("Summer", 10, "summer_item")
 
-rarities = [common, uncommon, rare, legendary, exotic, lootboxes, mythic, valentines, witch_queen, easter]
+rarities = [common, uncommon, rare, legendary, exotic, lootboxes, mythic, valentines, witch_queen, easter, summer]
 
 def get(search: str):
     search = search.upper()
