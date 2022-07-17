3b5360f6761c166646adbe22af1391305a585207
ChaosCantrip <james.donald.02@gmail.com>
2022-07-17 23:43:09 +0100

    Added databaseHandler.create_connection()

--- handlers/databaseHandler.py
+++ handlers/databaseHandler.py
@@ -1,2 +1,12 @@
 import mysql.connector
 import secret
+
+def create_connection():
+    connection = mysql.connector.connect(
+        host = secret.database.host,
+        user = secret.database.user,
+        password = secret.database.password,
+        database = secret.database.database
+    )
+
+    return connection
