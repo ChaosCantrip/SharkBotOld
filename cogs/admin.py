886429fc9a1d8ce6669dc4aa51e1396f086a316f
ChaosCantrip <james.donald.02@gmail.com>
2022-07-25 06:54:23 +0100

    Various fixes to make admin.py more readable and PEP8 compliant

--- cogs/admin.py
+++ cogs/admin.py
@@ -3,24 +3,23 @@ from discord.ext import tasks, commands
 from handlers import databaseHandler
 
 import secret
+
 if secret.testBot:
     import testids as ids
 else:
     import ids
 
-    
-    
+
 class Admin(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-    
 
     @commands.command()
     @commands.has_role(ids.roles["Mod"])
     async def migrate(self, ctx, *, newChannel: discord.VoiceChannel):
         currentChannel = ctx.author.voice.channel
-        if currentChannel == None:
+        if currentChannel is None:
             await ctx.send("You're not in a voice channel!")
             return
         members = list(currentChannel.members)
@@ -28,12 +27,11 @@ class Admin(commands.Cog):
             await member.move_to(newChannel)
         await ctx.send(f"Moved *{len(members)}* members from {currentChannel.mention} to {newChannel.mention}.")
 
-
     @commands.command()
     @commands.has_role(ids.roles["Mod"])
     async def summon(self, ctx, *, targetChannel: discord.VoiceChannel):
         currentChannel = ctx.author.voice.channel
-        if currentChannel == None:
+        if currentChannel is None:
             await ctx.send("You're not in a voice channel!")
             return
         members = list(targetChannel.members)
@@ -46,12 +44,12 @@ class Admin(commands.Cog):
     async def upload_all(self, ctx):
         databaseHandler.upload_all_members()
         await ctx.send("```Done!```")
-        
-        
-        
+
+
 def setup(bot):
     bot.add_cog(Admin(bot))
     print("Admin Cog loaded")
+
 
 def teardown(bot):
     print("Admin Cog unloaded")
