5ee47e0c03578819e29286472c7acbd644ef63f7
ChaosCantrip <james.donald.02@gmail.com>
2022-07-25 07:03:07 +0100

    Various fixes to make Member.py more readable and PEP8 compliant

--- definitions/Member.py
+++ definitions/Member.py
@@ -2,10 +2,11 @@ from definitions import SharkErrors, Item
 from handlers import databaseHandler
 import json
 
-class Member():
-    
+
+class Member:
+
     def __init__(self, member_data):
-        
+
         self.id = member_data["id"]
         self.balance = member_data["balance"]
         self.inventory = member_data["inventory"]
@@ -89,7 +90,7 @@ class Member():
 
     async def fetch_discord_member(self, bot):
         self.discordMember = bot.get_user(self.id)
-        if self.discordMember == None:
+        if self.discordMember is None:
             try:
                 self.discordMember = await bot.fetch_user(self.id)
             except:
@@ -99,23 +100,23 @@ class Member():
 
     def link_account(self, account):
         account = account.lower()
-        if self.linked_account != None:
+        if self.linked_account is not None:
             raise SharkErrors.AccountAlreadyLinkedError
-        
+
         usedAccounts = get_used_accounts()
         if account in usedAccounts:
             raise SharkErrors.AccountAlreadyInUseError
 
         usedAccounts.append(account)
         write_used_accounts(usedAccounts)
-        
+
         self.linked_account = account
         self.write_data()
 
     def unlink_account(self):
-        if self.linked_account == None:
+        if self.linked_account is None:
             raise SharkErrors.AccountNotLinkedError
-        
+
         usedAccounts = get_used_accounts()
         usedAccounts.remove(self.linked_account)
         write_used_accounts(usedAccounts)
@@ -144,7 +145,7 @@ class Member():
 
 
 class BlankMember(Member):
-    
+
     def __init__(self, member_id):
         self.id = int(member_id)
         self.balance = defaultvalues["balance"]
@@ -211,13 +212,13 @@ def write_used_accounts(accountList):
 
 
 defaultvalues = {
-    "id" : 1234,
-    "balance" : 0,
-    "inventory" : [],
-    "collection" : [],
-    "email" : None,
-    "counts" : 0
-    }
+    "id": 1234,
+    "balance": 0,
+    "inventory": [],
+    "collection": [],
+    "email": None,
+    "counts": 0
+}
 
 
 def update_data(data):
