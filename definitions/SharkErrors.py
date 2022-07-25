b6b9b36b333640ead02ca889c0c6f880683bbc4b
ChaosCantrip <james.donald.02@gmail.com>
2022-07-25 06:46:39 +0100

    Made SharkErrors.py PEP8 compliant

--- definitions/SharkErrors.py
+++ definitions/SharkErrors.py
@@ -1,30 +1,38 @@
-
 class SharkError(Exception):
     pass
+
 
 class MemberFileNotFoundError(SharkError):
     pass
 
+
 class AccountAlreadyLinkedError(SharkError):
     pass
+
 
 class AccountNotLinkedError(SharkError):
     pass
 
+
 class AccountAlreadyInUseError(SharkError):
     pass
+
 
 class ItemNotInInventoryError(SharkError):
     pass
 
+
 class ItemNotInCollectionError(SharkError):
     pass
+
 
 class CollectionNotFoundError(SharkError):
     pass
 
+
 class RarityNotFoundError(SharkError):
     pass
+
 
 class ItemNotFoundError(SharkError):
     pass
