47dac687768a5c7735b9b1a304ba501601a6b440
ChaosCantrip <james.donald.02@gmail.com>
2022-08-21 07:54:38 +0100

    Added Open All button to $buy command

--- cogs/collectibles.py
+++ cogs/collectibles.py
@@ -598,6 +598,40 @@ class Collectibles(commands.Cog):
         embed.add_field(name="**Available Items**", value=shopText)
         await ctx.reply(embed=embed, mention_author=False)
 
+    class BuyView(discord.ui.View):
+        def __init__(self, boughtItems: list[Item.Lootbox], memberid: int, embed: discord.Embed, timeout=180):
+            super().__init__(timeout=timeout)
+            self.boughtItems = boughtItems
+            self.memberid = memberid
+            self.embed = embed
+
+        @discord.ui.button(label="Open All")
+        async def openall_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+            member = Member.get(self.memberid)
+            button.disabled = True
+            print("a")
+            print(member.inventory.count(self.boughtItems[0].id))
+            print(len(self.boughtItems))
+            if member.inventory.count(self.boughtItems[0].id) < len(self.boughtItems):
+                print("b")
+                self.embed.color = discord.Color.red()
+                self.embed.add_field(name="Open All", value="It looks like the items you bought weren't in your inventory when you tried to open them!")
+                await interaction.response.edit_message(embed=self.embed, view=self)
+                return
+            openedText = ""
+            box = self.boughtItems[0]
+            for box in self.boughtItems:
+                item = box.roll()
+                if item.id in member.get_inventory():
+                    openedText += f":sparkles: {item.collection.get_icon(interaction.guild)} {item.name} :sparkles:\n"
+                else:
+                    openedText += f"{item.collection.get_icon(interaction.guild)} {item.name}\n"
+                member.remove_from_inventory(box)
+                member.add_to_inventory(item)
+            self.embed.add_field(name=f"Opened {len(self.boughtItems)}x {box.collection.get_icon(interaction.guild)} {box.name}", value=openedText)
+            await interaction.response.edit_message(embed=self.embed, view=self)
+
+
     @commands.command()
     async def buy(self, ctx, *, search):
         member = Member.get(ctx.author.id)
@@ -631,11 +665,15 @@ class Collectibles(commands.Cog):
         for i in range(num):
             member.add_balance(-1 * listing.price)
             member.add_to_inventory(item)
+
         embed = discord.Embed()
         embed.title = f"Bought {num}x {item.rarity.get_icon(self.server)} {item.name}"
         embed.description = f"You bought {num}x {item.rarity.get_icon(self.server)} {item.name} for *${listing.price * num}*"
         embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar.url)
-        await ctx.reply(embed=embed, mention_author=False)
+
+        view = self.BuyView([item] * num, ctx.author.id, embed)
+
+        await ctx.reply(embed=embed, view=view)
         member.upload_data()
 
     @commands.command(aliases=["gift"])
