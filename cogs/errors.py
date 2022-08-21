cc0bb853c60d515777b423a0044963b451c7fec0
ChaosCantrip <james.donald.02@gmail.com>
2022-08-21 08:38:16 +0100

    Added error handling for missing roles

--- cogs/errors.py
+++ cogs/errors.py
@@ -47,6 +47,9 @@ class Errors(commands.Cog):
             chaos = await self.bot.fetch_user(ids.users["Chaos"])
             await chaos.send("Couldn't connect to SIMP database.")
             return
+        if isinstance(error, commands.MissingRole):
+            await ctx.send("I'm afraid you don't have permission to do that!")
+            return
 
 
         errorType = type(error)
