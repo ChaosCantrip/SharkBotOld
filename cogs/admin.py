d0ef75135c80572a2bc8fe5a3aaddf3864bfb27f
ChaosCantrip <james.donald.02@gmail.com>
2022-08-21 08:29:29 +0100

    Fixed $migrate command error handling

--- cogs/admin.py
+++ cogs/admin.py
@@ -19,10 +19,10 @@ class Admin(commands.Cog):
     @commands.command()
     @commands.has_role(ids.roles["Mod"])
     async def migrate(self, ctx, *, newChannel: discord.VoiceChannel):
-        currentChannel = ctx.author.voice.channel
-        if currentChannel is None:
+        if ctx.author.voice is None:
             await ctx.send("You're not in a voice channel!")
             return
+        currentChannel = ctx.author.voice.channel
         members = list(currentChannel.members)
         for member in members:
             await member.move_to(newChannel)
