d76d3b25cae2987b6bcdf8eeb7ba49426d2e32cc
ChaosCantrip <james.donald.02@gmail.com>
2022-08-23 04:01:18 +0100

    Added function return annotations to functions in Cooldown class

--- definitions/Cooldown.py
+++ definitions/Cooldown.py
@@ -5,33 +5,33 @@ timeFormat = "%S:%M:%H/%d:%m:%Y"
 
 class Cooldown:
 
-    def __init__(self, name: str, expiry: str, duration: timedelta):
+    def __init__(self, name: str, expiry: str, duration: timedelta) -> None:
         self.name = name
         self.expiry = datetime.strptime(expiry, timeFormat)
         self.duration = duration
 
     @property
-    def expired(self):
+    def expired(self) -> bool:
         return datetime.now() > self.expiry
 
-    def extend(self):
+    def extend(self) -> None:
         self.expiry += self.duration
 
-    def reset(self):
+    def reset(self) -> None:
         self.expiry = datetime.now() + self.duration
 
     @property
-    def timestring(self):
+    def timestring(self) -> str:
         return datetime.strftime(self.expiry, timeFormat)
 
     @property
-    def timeremaining(self):
+    def timeremaining(self) -> timedelta:
         return datetime.now() - self.expiry
 
 
 class NewCooldown:
 
-    def __init__(self, name: str, duration: timedelta):
+    def __init__(self, name: str, duration: timedelta) -> None:
         self.name = name
         self.expiry = datetime.now() + duration
         self.duration = duration
