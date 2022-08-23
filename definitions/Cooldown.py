aa159c718d33eba8b0bfa9ab52bbf8d1c1ac3958
ChaosCantrip <james.donald.02@gmail.com>
2022-08-23 04:10:06 +0100

    Added Cooldown.timeremainingstr

--- definitions/Cooldown.py
+++ definitions/Cooldown.py
@@ -28,6 +28,46 @@ class Cooldown:
     def timeremaining(self) -> timedelta:
         return datetime.now() - self.expiry
 
+    @property
+    def timeremainingstr(self) -> str:
+        timeremaining = self.timeremaining + timedelta(days=0, hours=0, minutes=0, seconds=0)
+        seconds = timeremaining.seconds
+        minutes = timeremaining.minutes
+        hours = timeremaining.hours
+        days = timeremaining.days
+
+        outputString = ""
+        if days != 0:
+            if days == 1:
+                outputString += f"{days} day, "
+            else:
+                outputString += f"{days} days, "
+        if hours != 0:
+            if hours == 1:
+                outputString += f"{hours} hour, "
+            else:
+                outputString += f"{hours} hours, "
+        if minutes != 0:
+            if minutes == 1:
+                outputString += f"{minutes} minute, "
+            else:
+                outputString += f"{minutes} minutes, "
+        if outputString == "":
+            if seconds == 1:
+                outputString += f"{seconds} second "
+            else:
+                outputString += f"{seconds} seconds "
+        else:
+            outputString = outputString[:-2] + f" and {seconds} "
+            if seconds == 1:
+                outputString += f"second "
+            else:
+                outputString += f"seconds "
+
+        return outputString
+
+
+
 
 class NewCooldown:
 
