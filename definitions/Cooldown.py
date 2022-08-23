412bfc8a44d72d91f3439d8a9ad1df1560446760
ChaosCantrip <james.donald.02@gmail.com>
2022-08-23 03:21:21 +0100

    Added Cooldown.duration

--- definitions/Cooldown.py
+++ definitions/Cooldown.py
@@ -5,5 +5,7 @@ timeFormat = "%S:%M:%H/%d:%m:%Y"
 
 class Cooldown:
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, expiry: str, duration: timedelta):
         self.name = name
+        self.expiry = datetime.strptime(expiry, timeFormat)
+        self.duration = timedelta
