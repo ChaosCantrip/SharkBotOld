23cce6fc9fb11fe63001b6da29e69410467a051c
ChaosCantrip <james.donald.02@gmail.com>
2022-08-25 20:54:55 +0100

    Added $coinflip command

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import tasks, commands
 
 import secret
+import random
+from definitions import Member
 
 if secret.testBot:
     import testids as ids
@@ -13,6 +15,29 @@ class Fun(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+
+    @commands.hybrid_command()
+    async def coinflip(self, ctx, amount: int) -> None:
+        member = Member.get(ctx.author.id)
+        amount = int(amount)
+
+        if member.get_balance() < amount:
+            await ctx.send(f"You don't have ${amount} to bet!")
+            return
+
+        member.add_balance(-amount)
+        roll = random.randint(1,2)
+        if roll == 1:   ## Win
+            member.add_balance(2*amount)
+            await ctx.send(f"You win! You bet ${amount} and won ${2*amount}!")
+        else: # Loss
+            mercyroll = random.randint(1,8)
+            if mercyroll == 1:
+                member.add_balance(amount)
+                await ctx.send(f"You lose! You lost ${amount}, but I'm feeling nice, so I'll let you have it back!")
+            else:
+                await ctx.send(f"You lose! You bet ${amount} and lost!")
 
 
 async def setup(bot):
