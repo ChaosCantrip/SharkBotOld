0582b5db0999b3fbc7aec8fef056e57cbd4ad679
ChaosCantrip <james.donald.02@gmail.com>
2022-08-26 03:26:31 +0100

    Moved $summon and $migrate to Voice Cog

--- cogs/admin.py
+++ cogs/admin.py
@@ -17,30 +17,6 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command()
-    @commands.has_role(ids.roles["Mod"])
-    async def migrate(self, ctx, *, newchannel: discord.VoiceChannel):
-        if ctx.author.voice is None:
-            await ctx.send("You're not in a voice channel!")
-            return
-        currentChannel = ctx.author.voice.channel
-        members = list(currentChannel.members)
-        for member in members:
-            await member.move_to(newchannel)
-        await ctx.send(f"Moved *{len(members)}* members from {currentChannel.mention} to {newchannel.mention}.")
-
-    @commands.hybrid_command()
-    @commands.has_role(ids.roles["Mod"])
-    async def summon(self, ctx, *, targetchannel: discord.VoiceChannel):
-        if ctx.author.voice is None:
-            await ctx.send("You're not in a voice channel!")
-            return
-        members = list(targetchannel.members)
-        currentChannel = ctx.author.voice.channel
-        for member in members:
-            await member.move_to(currentChannel)
-        await ctx.send(f"Moved *{len(members)}* members from {targetchannel.mention} to {currentChannel.mention}.")
-
     @commands.command()
     @commands.is_owner()
     async def upload_all(self, ctx):
--- cogs/voice.py
+++ cogs/voice.py
@@ -14,6 +14,30 @@ class Voice(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.hybrid_command()
+    @commands.has_role(ids.roles["Mod"])
+    async def migrate(self, ctx, *, newchannel: discord.VoiceChannel):
+        if ctx.author.voice is None:
+            await ctx.send("You're not in a voice channel!")
+            return
+        currentChannel = ctx.author.voice.channel
+        members = list(currentChannel.members)
+        for member in members:
+            await member.move_to(newchannel)
+        await ctx.send(f"Moved *{len(members)}* members from {currentChannel.mention} to {newchannel.mention}.")
+
+    @commands.hybrid_command()
+    @commands.has_role(ids.roles["Mod"])
+    async def summon(self, ctx, *, targetchannel: discord.VoiceChannel):
+        if ctx.author.voice is None:
+            await ctx.send("You're not in a voice channel!")
+            return
+        members = list(targetchannel.members)
+        currentChannel = ctx.author.voice.channel
+        for member in members:
+            await member.move_to(currentChannel)
+        await ctx.send(f"Moved *{len(members)}* members from {targetchannel.mention} to {currentChannel.mention}.")
+
 
 async def setup(bot):
     await bot.add_cog(Voice(bot))
