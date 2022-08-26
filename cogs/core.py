7614edb7b9bf20237bc91f997592018f7f6ec5fb
ChaosCantrip <james.donald.02@gmail.com>
2022-08-26 01:43:08 +0100

    Removed $getdata command

--- cogs/core.py
+++ cogs/core.py
@@ -25,11 +25,6 @@ class Core(commands.Cog):
         await ctx.send(f"Pong! t={(datetime.datetime.now() - ctx.message.created_at).total_seconds() * 1000}ms")
 
     @commands.command()
-    @commands.has_role(ids.roles["Mod"])
-    async def getdata(self, ctx):
-        await ctx.send("Working!", file=discord.File("data/memberdata.json"))
-
-    @commands.command()
     @commands.is_owner()
     async def send(self, ctx, channel: discord.TextChannel, *, text):
         await channel.send(text)
