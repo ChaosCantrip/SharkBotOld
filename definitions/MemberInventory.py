df35ad585cd990b29f0287dc5b75887e0e00acf3
ChaosCantrip <james.donald.02@gmail.com>
2022-09-02 12:05:39 +0100

    Added MemberInventory.itemids

--- definitions/MemberInventory.py
+++ definitions/MemberInventory.py
@@ -6,3 +6,11 @@ class MemberInventory:
     def __init__(self, member: Member.Member, itemids: list[Item.Item]) -> None:
         self.member = member
         self._items = [Item.get(itemid) for itemid in itemids]
+
+    @property
+    def items(self):
+        return list(self._items)
+
+    @property
+    def itemids(self):
+        return [item.id for item in self._items]
