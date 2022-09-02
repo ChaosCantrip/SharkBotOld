a0745266e29f3482ef599f5337a1eb9f485f7159
ChaosCantrip <james.donald.02@gmail.com>
2022-09-02 12:03:22 +0100

    Added MemberInventory class constructor

--- definitions/MemberInventory.py
+++ definitions/MemberInventory.py
@@ -1,2 +1,8 @@
+from definitions import Member, Item
+
+
 class MemberInventory:
-    pass
+
+    def __init__(self, member: Member.Member, itemids: list[Item.Item]) -> None:
+        self.member = member
+        self._items = [Item.get(itemid) for itemid in itemids]
