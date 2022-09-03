f14816ac75583f228ecbe8f42855b332103cfee0
ChaosCantrip <james.donald.02@gmail.com>
2022-09-03 01:11:46 +0100

    Added daily and weekly counting missions

--- definitions/Mission.py
+++ definitions/Mission.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
+from definitions import Item, SharkErrors
 from typing import Union
 
 
 class Mission:
 
-    def __init__(self, id: str, name: str, description: str, quota: int, duration: timedelta, reward):
+    def __init__(self, id: str, name: str, description: str, quota: int, duration: timedelta, reward: Item.Item):
         self.id = id
         self.name = name
         self.description = description
@@ -24,3 +25,23 @@ class MemberMission:
 
 class MemberMissions:
     pass
+
+
+missions = [
+    Mission(
+        id="dailyCount",
+        name="Daily Count",
+        description="Count 3 times a day",
+        quota=3,
+        duration=timedelta(days=1),
+        reward=Item.get("LOOT1")
+    ),
+    Mission(
+        id="weeklyCount",
+        name="Weekly Count",
+        description="Count 10 times a week",
+        quota=10,
+        duration=timedelta(weeks=1),
+        reward=Item.get("LOOT5")
+    )
+]
