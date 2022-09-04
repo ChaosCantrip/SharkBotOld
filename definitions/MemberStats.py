2001bb2b4cf5b111d64d8f9eaa5247a36328f53a
ChaosCantrip <james.donald.02@gmail.com>
2022-09-04 12:44:39 +0100

    Added Member.stats.coinflipWinRate

--- definitions/MemberStats.py
+++ definitions/MemberStats.py
@@ -1,16 +1,35 @@
 class MemberStats:
 
     def __init__(self, data: dict[str, int]):
-        self.coinflipWins: int = data["coinflipWins"] if "coinflipWins" in data else 0
-        self.coinflipLosses: int = data["coinflipLosses"] if "coinflipLosses" in data else 0
+        self._coinflipWins: int = data["coinflipWins"] if "coinflipWins" in data else 0
+        self._coinflipLosses: int = data["coinflipLosses"] if "coinflipLosses" in data else 0
+        self.coinflipWinRate: float = round(self._coinflipWins / (self._coinflipLosses + self._coinflipWins), 2)
         self.claims: int = data["claims"] if "claims" in data else 0
         self.incorrectCounts: int = data["incorrectCounts"] if "incorrectCounts" in data else 0
 
     @property
     def data(self) -> dict[str, int]:
         return {
-            "coinflipWins": self.coinflipWins,
-            "coinflipLosses": self.coinflipLosses,
+            "coinflipWins": self._coinflipWins,
+            "coinflipLosses": self._coinflipLosses,
             "claims": self.claims,
             "incorrectCounts": self.incorrectCounts
         }
+
+    @property
+    def coinflipWins(self) -> int:
+        return self._coinflipWins
+
+    @coinflipWins.setter
+    def coinflipWins(self, value: int) -> None:
+        self._coinflipWins = value
+        self.coinflipWinRate = round(self._coinflipWins / (self._coinflipLosses + self._coinflipWins), 2)
+
+    @property
+    def coinflipLosses(self) -> int:
+        return self._coinflipLosses
+
+    @coinflipWins.setter
+    def coinflipLosses(self, value: int) -> None:
+        self._coinflipLosses = value
+        self.coinflipWinRate = round(self._coinflipWins / (self._coinflipLosses + self._coinflipWins), 2)
