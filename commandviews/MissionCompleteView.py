c6ab10995388ca02c750fa6cd60ae6de9527e06a
ChaosCantrip <james.donald.02@gmail.com>
2022-09-04 04:41:04 +0100

    Removed Member import from MissionCompleteView

--- commandviews/MissionCompleteView.py
+++ commandviews/MissionCompleteView.py
@@ -1,15 +1,15 @@
 import discord
 from discord.ext import commands
 
-from definitions import Item, Member
+from definitions import Item
 from .OpenButton import OpenButton
 
 
 class MissionCompleteView(discord.ui.View):
-    def __init__(self, boxes: list[Item.Lootbox], memberid: int, embed: discord.Embed, timeout=180):
+    def __init__(self, boxes: list[Item.Lootbox], member, embed: discord.Embed, timeout=180):
         super().__init__(timeout=timeout)
         self.boxes = boxes
-        self.member = Member.get(memberid)
+        self.member = member
         self.embed = embed
         self.add_item(OpenButton(self.member, self.embed, self.boxes))
 
