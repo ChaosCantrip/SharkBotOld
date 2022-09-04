137b7dc5e2f3698a6d912c21ca0a1cc65af63ca4
ChaosCantrip <james.donald.02@gmail.com>
2022-09-04 03:24:35 +0100

    Moved OpenButton to own class file

---
+++ commandviews/OpenButton.py
@@ -0,0 +1,52 @@
+import discord
+from definitions import Member, Item
+from .SellButton import SellButton
+
+
+class OpenButton(discord.ui.Button):
+    def __init__(self, member: Member.Member, embed: discord.Embed, boxes: list[Item.Lootbox], label="Open All",
+                 **kwargs):
+        super().__init__(label=label, **kwargs)
+        self.member = member
+        self.embed = embed
+        self.boxes = boxes
+
+    async def callback(self, interaction: discord.Interaction) -> None:
+        if interaction.user.id != self.member.id:
+            return
+
+        self.disabled = True
+
+        if any([self.member.inventory.items.count(box) < self.boxes.count(box) for box in set(self.boxes)]):
+            self.embed.add_field(
+                name="Open All Failed",
+                value="It looks like the boxes aren't in your inventory any more!",
+                inline=False
+            )
+            self.embed.colour = discord.Colour.red()
+            await interaction.response.edit_message(embed=self.embed, view=self.view)
+            return
+
+        allOpenedItems = []
+        a = {box: self.boxes.count(box) for box in set(self.boxes)}
+        for box, count in {box: self.boxes.count(box) for box in set(self.boxes)}.items():
+            openedItems = [boxToOpen.roll() for boxToOpen in [box] * count]
+            allOpenedItems += openedItems
+
+            for item in openedItems:
+                self.member.inventory.remove(box)
+                self.member.inventory.add(item)
+
+            self.embed.add_field(
+                name=f"Opened {count}x {box.rarity.icon} {box.name}",
+                value="\n".join(
+                    [f"{item.rarity.icon} {item.name}{':sparkles:' if not self.member.inventory.contains(item) else ''}"
+                        for item in openedItems]
+                ),
+                inline=False
+            )
+
+        self.view.add_item(SellButton(self.member, self.embed, allOpenedItems))
+        await interaction.response.edit_message(embed=self.embed, view=self.view)
+
+        self.member.write_data()
