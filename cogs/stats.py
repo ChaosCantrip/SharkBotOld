4e2b2dde58b9a01d8ed48f3612025c5fb830970a
ChaosCantrip <james.donald.02@gmail.com>
2022-09-04 12:57:01 +0100

    Added stats Cog and command

---
+++ cogs/stats.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import tasks, commands
+
+import secret
+from definitions import Member
+
+if secret.testBot:
+	import testids as ids
+else:
+	import ids
+
+
+class Stats(commands.Cog):
+
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.hybrid_command()
+	async def stats(self, ctx: commands.Context):
+		member = Member.get(ctx.author.id)
+
+		embed = discord.Embed()
+		embed.title = "Your Stats"
+		embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar.url)
+		embed.set_thumbnail(url=ctx.author.avatar.url)
+		embed.description = f"Counts: `{member.get_counts()}`"
+		embed.description += f"\nIncorrect Counts: `{member.stats.incorrectCounts}`"
+		embed.description += f"\nCoinflip KDA: `{member.stats.coinflipkda}`"
+		embed.description += f"\nCoinflip Win Rate: `{member.stats.coinflipwinrate}%`"
+
+		await ctx.send(embed=embed)
+
+
+
+async def setup(bot):
+	await bot.add_cog(Stats(bot))
+	print("Stats Cog loaded")
+
+
+async def teardown(bot):
+	print("Stats Cog unloaded")
+	await bot.remove_cog(Stats(bot))
