05c91f3b7dc6b155e047ee3b3fa0c9aeddd2baa0
ChaosCantrip <james.donald.02@gmail.com>
2022-09-04 12:51:40 +0100

    Added Changed winrate to a property

--- definitions/MemberStats.py
+++ definitions/MemberStats.py
@@ -1,41 +1,26 @@
 class MemberStats:
 
     def __init__(self, data: dict[str, int]):
-        self._coinflipWins: int = data["coinflipWins"] if "coinflipWins" in data else 0
-        self._coinflipLosses: int = data["coinflipLosses"] if "coinflipLosses" in data else 0
+        self.coinflipWins: int = data["coinflipWins"] if "coinflipWins" in data else 0
+        self.coinflipLosses: int = data["coinflipLosses"] if "coinflipLosses" in data else 0
         self.coinflipMercies: int = data["coinflipMercies"] if "coinflipMercies" in data else 0
-        self.coinflipWinRate: float = round(self._coinflipWins / (self._coinflipLosses + self._coinflipWins), 2)
         self.claims: int = data["claims"] if "claims" in data else 0
         self.incorrectCounts: int = data["incorrectCounts"] if "incorrectCounts" in data else 0
 
     @property
     def data(self) -> dict[str, int]:
         return {
-            "coinflipWins": self._coinflipWins,
-            "coinflipLosses": self._coinflipLosses,
+            "coinflipWins": self.coinflipWins,
+            "coinflipLosses": self.coinflipLosses,
             "coinflipMercies": self.coinflipMercies,
             "claims": self.claims,
             "incorrectCounts": self.incorrectCounts
         }
 
     @property
-    def coinflipWins(self) -> int:
-        return self._coinflipWins
-
-    @coinflipWins.setter
-    def coinflipWins(self, value: int) -> None:
-        self._coinflipWins = value
-        self.coinflipWinRate = round(self._coinflipWins / (self._coinflipLosses + self._coinflipWins), 2)
-
-    @property
-    def coinflipLosses(self) -> int:
-        return self._coinflipLosses
-
-    @coinflipWins.setter
-    def coinflipLosses(self, value: int) -> None:
-        self._coinflipLosses = value
-        self.coinflipWinRate = round(self._coinflipWins / (self._coinflipLosses + self._coinflipWins), 2)
+    def coinflipwinrate(self) -> float:
+        return round(self.coinflipWins / (self.coinflipWins + self.coinflipLosses), 2)
 
     @property
     def coinflipkda(self) -> str:
-        return f"{self._coinflipWins}|{self._coinflipLosses}|{self.coinflipMercies}"
+        return f"{self.coinflipWins}|{self.coinflipLosses}|{self.coinflipMercies}"
