91270dcc83c47257cba3278d7af004cb734325cb
ChaosCantrip <james.donald.02@gmail.com>
2022-09-08 22:32:26 +0100

    Fixed incorrect function annotation in MemberCollection

--- definitions/MemberCollection.py
+++ definitions/MemberCollection.py
@@ -4,7 +4,7 @@ from typing import Union
 
 class MemberCollection:
 
-    def __init__(self, member, itemids: list[Item.Item]) -> None:
+    def __init__(self, member, itemids: list[str]) -> None:
         self.member = member
         self._items = [Item.get(itemid) for itemid in itemids]
 
