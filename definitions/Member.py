9cc924ab38d4ec21179d02ebc75494dc90460683
ChaosCantrip <james.donald.02@gmail.com>
2022-09-11 23:59:47 +0100

    Re-enabled Member.upload_data()

--- definitions/Member.py
+++ definitions/Member.py
@@ -34,7 +34,7 @@ class Member:
         self.lastClaimedBirthday = member_data["lastClaimedBirthday"]
         self.stats = MemberStats.MemberStats(member_data["stats"])
 
-    def write_data(self, upload: bool = False) -> None:
+    def write_data(self, upload: bool = True) -> None:
 
         member_data = {
             "id": self.id,
