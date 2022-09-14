7a05c32ff73c044befbc86b2d844d720c98d6b3a
ChaosCantrip <james.donald.02@gmail.com>
2022-09-14 17:57:18 +0100

    Added websiteChecker.get_site()

--- handlers/websiteChecker.py
+++ handlers/websiteChecker.py
@@ -3,3 +3,8 @@ import hashlib
 
 url = "https://www.imperial.ac.uk/students/accommodation/current-residents/vacancies/"
 headers = {'User-Agent': 'Mozilla/5.0'}
+
+
+def get_site() -> str:
+    response = requests.get(url, headers=headers)
+    return str(response.content)
