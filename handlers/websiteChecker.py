5505a8b268a109c17fb07219279cfa74a748fee1
ChaosCantrip <james.donald.02@gmail.com>
2022-09-14 17:58:01 +0100

    Added websiteChecker.convert_to_hash()

--- handlers/websiteChecker.py
+++ handlers/websiteChecker.py
@@ -8,3 +8,7 @@ headers = {'User-Agent': 'Mozilla/5.0'}
 def get_site() -> str:
     response = requests.get(url, headers=headers)
     return str(response.content)
+
+
+def convert_to_hash(string: str) -> str:
+    return hashlib.sha224(string).hexdigest()
