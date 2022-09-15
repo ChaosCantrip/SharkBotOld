5c8f90bcdf2ec8a0ccd821cea42348881cae207b
ChaosCantrip <james.donald.02@gmail.com>
2022-09-15 23:30:46 +0100

    Fixed fetching of champions and shields for LostSectors

--- SharkBot/destiny/LostSector.py
+++ SharkBot/destiny/LostSector.py
@@ -6,5 +6,5 @@ class LostSector:
 
     def __init__(self, data: dict[str, Union[str, dict[str, list[str]]]]):
         self.name: str = data["name"]
-        self.champions: list[Champion.Champion] = [Champion.get(champion) for champion in data["champions"]]
-        self.shields: list[Shield.Shield] = [Shield.get(shield) for shield in data["shields"]]
+        self.champions: list[Champion.Champion] = [Champion.get(champion) for champion in data["legend"]["champions"]]
+        self.shields: list[Shield.Shield] = [Shield.get(shield) for shield in data["legend"]["shields"]]
