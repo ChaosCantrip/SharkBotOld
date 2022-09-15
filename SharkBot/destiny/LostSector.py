7c149b3204c1b0c5e7d2fe7482185b60444a2fe3
ChaosCantrip <james.donald.02@gmail.com>
2022-09-15 23:17:45 +0100

    Completed LostSector constructor

--- SharkBot/destiny/LostSector.py
+++ SharkBot/destiny/LostSector.py
@@ -1,7 +1,10 @@
 from typing import Union
+from SharkBot.destiny import Champion, Shield
 
 
 class LostSector:
 
     def __init__(self, data: dict[str, Union[str, dict[str, list[str]]]]):
-        pass
+        self.name: str = data["name"]
+        self.champions: list[Champion.Champion] = [Champion.get(champion) for champion in data["champions"]]
+        self.shields: list[Shield.Shield] = [Shield.get(shield) for shield in data["shields"]]
