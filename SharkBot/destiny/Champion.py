d8a6487f3979a040e1cc2f830b0a0a77fa6d1aa5
ChaosCantrip <james.donald.02@gmail.com>
2022-09-15 23:04:46 +0100

    Added 3 Champion Types, Placeholder Icons

--- SharkBot/destiny/Champion.py
+++ SharkBot/destiny/Champion.py
@@ -3,3 +3,19 @@ class Champion:
     def __init__(self, name: str, icon: str):
         self.name = name
         self.icon = icon
+
+
+champions = [
+    Champion(
+        name="Barrier",
+        icon="(Barrier Icon)"
+    ),
+    Champion(
+        name="Overload",
+        icon="(Overload Icon)"
+    ),
+    Champion(
+        name="Unstoppable",
+        icon="(Unstoppable Icon)"
+    )
+]
