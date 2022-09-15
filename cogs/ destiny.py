d7931d4ee4d1070aa9b5a295cf87670ac131a4f3
ChaosCantrip <james.donald.02@gmail.com>
2022-09-15 23:36:51 +0100

    Added Destiny Cog

---
+++ cogs/ destiny.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import tasks, commands
+
+import secret
+
+if secret.testBot:
+    import testids as ids
+else:
+    import ids
+
+
+class Destiny(commands.Cog):
+
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+
+async def setup(bot):
+    await bot.add_cog(Destiny(bot))
+    print("Destiny Cog loaded")
+
+
+async def teardown(bot):
+    print("Destiny Cog unloaded")
+    await bot.remove_cog(Destiny(bot))
