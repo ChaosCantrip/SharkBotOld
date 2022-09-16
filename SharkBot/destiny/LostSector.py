f46429d6e0c309e2e1c7e1ddb4e82d3db5e581f4
ChaosCantrip <james.donald.02@gmail.com>
2022-09-16 01:13:18 +0100

    Added LostSector.destination

--- SharkBot/destiny/LostSector.py
+++ SharkBot/destiny/LostSector.py
@@ -8,6 +8,7 @@ class LostSector:
 
     def __init__(self, data: dict[str, Union[str, dict[str, list[str]]]]):
         self.name: str = data["name"]
+        self.destination: str = data["destination"]
         self.champions: list[Champion.Champion] = [Champion.get(champion) for champion in data["legend"]["champions"]]
         self.shields: list[Shield.Shield] = [Shield.get(shield) for shield in data["legend"]["shields"]]
 
