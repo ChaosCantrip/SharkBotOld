a8d7bcfd8c899d501a6a16daf379f2d7d46b8ebf
ChaosCantrip <james.donald.02@gmail.com>
2022-09-19 07:37:46 +0100

    Added testBot handling of Firestore

--- SharkBot/Handlers/firestoreHandler.py
+++ SharkBot/Handlers/firestoreHandler.py
@@ -1,14 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import secret
 
-# Use a service account
-cred = credentials.Certificate('firestorecred.sbignore.json')
-firebase_admin.initialize_app(cred)
 
-db = firestore.client()
+if secret.allowFirestore:
+    cred = credentials.Certificate('firestorecred.sbignore.json')
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
 
 
 def upload_member(data: dict):
+    if secret.allowFirestore:
+        return
     doc_ref = db.collection(u'memberdata').document(str(data["id"]))
     doc_ref.set(data)
