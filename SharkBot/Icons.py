7121c5b5c8946b6227d6df02bc12598a777413b7
ChaosCantrip <james.donald.02@gmail.com>
2022-09-19 06:44:20 +0100

    Changed Collection icons to new ones

--- SharkBot/Icons.py
+++ SharkBot/Icons.py
@@ -1,13 +1,13 @@
 Collections = {
-    "witch_queen_item": "<:witch_queen_item:945523298777837628>",
-    "easter_item": "<:easter_item:964988672451698759>",
-    "exotic_item": "<:exotic_item:964988672669786163>",
-    "common_item": "<:common_item:964988672699150346>",
-    "legendary_item": "<:legendary_item:964988672724332594>",
-    "mythic_item": "<:mythic_item:964988672732700712>",
-    "uncommon_item": "<:uncommon_item:964988672749502484>",
-    "rare_item": "<:rare_item:964988672774651975>",
-    "valentines_item": "<:valentines_item:964996788341858304>",
-    "lootboxes_item": "<:lootboxes_item:964996788387971162>",
-    "summer_item": "<:summer_item:994990387569627166>"
+    "witch_queen_item": "<:witch_queen_item:945523595629694996>",
+    "exotic_item": "<:exotic_item:964988904035983432>",
+    "mythic_item": "<:mythic_item:964988904124084315>",
+    "common_item": "<:common_item:964988904371535902>",
+    "easter_item": "<:easter_item:964988904388321320>",
+    "rare_item": "<:rare_item:964988904421867591>",
+    "legendary_item": "<:legendary_item:964988904459604028>",
+    "uncommon_item": "<:uncommon_item:964988904535105566>",
+    "valentines_item": "<:valentines_item:964996933234073600>",
+    "lootboxes_item": "<:lootboxes_item:964996933351538790>",
+    "summer_item": "<:summer_item:1012587024248754176>"
 }
