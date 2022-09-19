0aed4c47bf61ccb7ba38af0def89a0cde7a6cc4d
ChaosCantrip <james.donald.02@gmail.com>
2022-09-20 00:38:50 +0100

    Added 18:00 to season times

--- SharkBot/Destiny/Season.py
+++ SharkBot/Destiny/Season.py
@@ -1,2 +1,14 @@
+from datetime import datetime, timedelta
+
+
 class Season:
-    pass
+
+    def __init__(self, name: str, number: int, start: str, end: str, icon: str) -> None:
+        self.name = name
+        self.number = number
+        self.start = datetime.strptime(start, "%d/%m/%Y")
+        self.start += timedelta(hours=18)
+        self.end = datetime.strptime(end, "%d/%m/%Y")
+        self.end += timedelta(hours=18)
+        self.icon = icon
+
