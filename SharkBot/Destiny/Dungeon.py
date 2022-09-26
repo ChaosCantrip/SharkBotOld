5ed024a86ed59d1e9aaea4f0d4df33a0d9ab30bf
ChaosCantrip <james.donald.02@gmail.com>
2022-09-27 00:10:13 +0100

    Added _DungeonData class

--- SharkBot/Destiny/Dungeon.py
+++ SharkBot/Destiny/Dungeon.py
@@ -1,3 +1,11 @@
+from typing import TypedDict
+
+
+class _DungeonData(TypedDict):
+    name: str
+    destination: str
+
+
 class Dungeon:
 
     def __init__(self, name: str, destination: str) -> None:
