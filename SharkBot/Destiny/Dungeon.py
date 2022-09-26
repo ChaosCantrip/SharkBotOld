1c187aad0fc8570d8e87a4465e6273fe17ec46b7
ChaosCantrip <james.donald.02@gmail.com>
2022-09-27 00:25:34 +0100

    Added Dungeon.get_current()

--- SharkBot/Destiny/Dungeon.py
+++ SharkBot/Destiny/Dungeon.py
@@ -1,6 +1,6 @@
 import json
 from typing import TypedDict
-from datetime import date
+from datetime import date, datetime, timedelta
 
 from SharkBot import Destiny
 
@@ -42,3 +42,13 @@ with open("data/static/destiny/dungeons/rotation.json", "r") as infile:
 seasonal: Dungeon = get(rotationData["seasonal"])
 rotation: list[Dungeon] = [get(dungeonName) for dungeonName in rotationData["featured"]]
 rotationStart = date(year=2022, month=8, day=23)
+
+
+def get_current() -> Dungeon:
+    dtnow = datetime.utcnow()
+    if dtnow.time() < Destiny.resetTime:
+        dtnow = dtnow - timedelta(days=1)
+    days = (dtnow - rotationStart).days
+    weeks = int(days / 7)
+    position = weeks % len(rotation)
+    return rotation[position]
