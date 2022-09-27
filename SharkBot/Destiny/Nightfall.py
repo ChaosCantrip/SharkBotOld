e5ca2fbcb66adee962048f99385aa70a6f850f22
ChaosCantrip <james.donald.02@gmail.com>
2022-09-27 14:49:51 +0100

    Added Nightfall properties

--- SharkBot/Destiny/Nightfall.py
+++ SharkBot/Destiny/Nightfall.py
@@ -14,3 +14,19 @@ class Nightfall:
         self.destination = destination
         self.legend = Destiny.LostSector.Difficulty(**legend)
         self.master = Destiny.LostSector.Difficulty(**master)
+
+    @property
+    def champion_types(self) -> set[Destiny.Champion.Champion]:
+        return set(self.legend.champion_types + self.master.champion_types)
+
+    @property
+    def shield_types(self) -> set[Destiny.Shield.Shield]:
+        return set(self.legend.shield_types + self.master.shield_types)
+
+    @property
+    def champion_list(self) -> str:
+        return ", ".join(champion.text for champion in self.champion_types)
+
+    @property
+    def shield_list(self) -> str:
+        return ", ".join(shield.text for shield in self.shield_types)
