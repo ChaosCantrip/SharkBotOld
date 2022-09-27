311a0fb97e5fb9ed64332f624e25448057a76cd4
ChaosCantrip <james.donald.02@gmail.com>
2022-09-27 14:48:38 +0100

    Added Nightfall Class

--- SharkBot/Destiny/Nightfall.py
+++ SharkBot/Destiny/Nightfall.py
@@ -5,3 +5,12 @@ from typing import TypedDict
 class _DifficultyData(TypedDict):
     champions: dict[str, int]
     shields: dict[str, int]
+
+
+class Nightfall:
+
+    def __init__(self, name: str, destination: str, legend: _DifficultyData, master: _DifficultyData):
+        self.name = name
+        self.destination = destination
+        self.legend = Destiny.LostSector.Difficulty(**legend)
+        self.master = Destiny.LostSector.Difficulty(**master)
