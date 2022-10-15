7e56bb748f2b870cf7287f024c68aeba76001f8a
ChaosCantrip <james.donald.02@gmail.com>
2022-10-15 11:28:36 +0100

    Added description to Utils.get_dir_filepaths() and refactored dir to directory to not shadow built in name

--- SharkBot/Utils.py
+++ SharkBot/Utils.py
@@ -6,5 +6,10 @@ def roll_probability(probability: int) -> bool:
     return random.randint(0, probability) == probability
 
 
-def get_dir_filepaths(dir: str) -> list[str]:
-    return [f"{dir}/{filename}" for filename in os.listdir(dir)]
+def get_dir_filepaths(directory: str) -> list[str]:
+    """
+    Returns a list of all files in the directory, including the path to the directory
+
+    :param directory: Directory to be listed
+    """
+    return [f"{directory}/{filename}" for filename in os.listdir(directory)]
