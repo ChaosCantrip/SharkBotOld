1a280bb333b7a7d12af11d5452c182740572c4ef
ChaosCantrip <james.donald.02@gmail.com>
2022-10-15 11:22:36 +0100

    Added SharkBot.Utils.get_dir_filepath()

--- SharkBot/Utils.py
+++ SharkBot/Utils.py
@@ -1,5 +1,10 @@
 import random
+import os
 
 
 def roll_probability(probability: int) -> bool:
     return random.randint(0, probability) == probability
+
+
+def get_dir_filepaths(dir: str) -> list[str]:
+    return [f"{dir}/{filename}" for filename in os.listdir(dir)]
