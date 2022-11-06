06c19a589d660a79435cec18174dd1cd02ad4aa2
James Donald <dev@chaoscantrip.com>
2022-11-06 23:42:07 +0000

    Added SharkBot.Utils.split_embeds()

--- SharkBot/Utils.py
+++ SharkBot/Utils.py
@@ -2,6 +2,8 @@ import random
 import os
 from typing import Union
 
+import discord
+
 
 def roll_probability(probability: int) -> bool:
     return random.randint(0, probability) == probability
@@ -21,3 +23,20 @@ def get_dir_filepaths(directory: str, extension: Union[str, None] = None) -> lis
         if extension[0] != ".":
             extension = f".{extension}"
         return [f"{directory}/{filename}" for filename in os.listdir(directory) if filename.endswith(extension)]
+
+
+def split_embeds(embed: discord.Embed) -> list[discord.Embed]:
+    embeds = [embed.copy()]
+    embeds[-1].clear_fields()
+
+    for field in embed.fields:
+        if len(embeds[-1]) + len(field.name) + len(field.value) > 5500 or len(embeds[-1].fields) == 25:
+            embeds.append(embed.copy())
+            embeds[-1].clear_fields()
+        embeds[-1].add_field(
+            name=field.name,
+            value=field.value,
+            inline=field.inline
+        )
+
+    return embeds
