858d835664ad99b97568b437d625bbf02b423d43
James Donald <dev@chaoscantrip.com>
2022-11-07 12:01:09 +0000

    Filled Lootpool constructor

--- SharkBot/Lootpool.py
+++ SharkBot/Lootpool.py
@@ -7,8 +7,11 @@ import SharkBot
 class Lootpool:
     lootpools = []
 
-    def __init__(self):
-        pass
+    def __init__(self, lootpool_id: str, table: dict[str, str]):
+        self.id = lootpool_id
+        self._raw_nodes = list(table.keys())
+        self._weightings = list(float(weight) for weight in table.values())
+        self._built_nodes = None
 
     @classmethod
     def get(cls, lootpool_id: str) -> Self:
