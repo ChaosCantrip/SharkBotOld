f84d1cf4701785386e30b469cc30ed5871d171d0
James Donald <dev@chaoscantrip.com>
2022-11-07 12:05:54 +0000

    Added Lootpool.build()

--- SharkBot/Lootpool.py
+++ SharkBot/Lootpool.py
@@ -13,6 +13,19 @@ class Lootpool:
         self._weightings = list(float(weight) for weight in table.values())
         self._built_nodes = None
 
+    def build(self) -> None:
+        self._built_nodes = []
+        for raw_node in self._raw_nodes:
+            node_type, node_value = raw_node.split(":")
+            if node_type == "lootpool":
+                self._built_nodes.append(self.get(node_value))
+            elif node_type == "collection":
+                self._built_nodes.append(SharkBot.Collection.get(node_value))
+            elif node_type == "item":
+                self._built_nodes.append(SharkBot.Item.get(node_value))
+            else:
+                raise SharkBot.Errors.UnknownLootpoolNodeType(self, raw_node)
+
     @classmethod
     def get(cls, lootpool_id: str) -> Self:
         for lootpool in cls.lootpools:
