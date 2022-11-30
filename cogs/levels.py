01a462cfacd7d79ddbcb9e9605db9564e83271c3
James Donald <dev@chaoscantrip.com>
2022-11-30 09:49:20 +0000

    Added $level command

--- cogs/levels.py
+++ cogs/levels.py
@@ -1,11 +1,24 @@
 import discord
 from discord.ext import tasks, commands
 
+from SharkBot import Member
+
 
 class Levels(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+
+    @commands.hybrid_command()
+    async def level(self, ctx: commands.Context):
+        member = Member.get(ctx.author.id)
+
+        embed = discord.Embed()
+        embed.title = "Level"
+        embed.set_thumbnail(url=ctx.author.avatar.url)
+        embed.description = f"You are **Level {member.xp.level}** with `{member.xp.xp} xp`"
+
+        await ctx.reply(embed=embed)
 
 
 async def setup(bot):
