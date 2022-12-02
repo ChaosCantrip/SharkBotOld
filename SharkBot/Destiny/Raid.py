5a3a7d7afc6cc4e58f9c8723d63e3667af0acc2c
James Donald <dev@chaoscantrip.com>
2022-12-02 22:28:53 +0000

    Refactored Raid into a class based import

--- SharkBot/Destiny/Raid.py
+++ SharkBot/Destiny/Raid.py
@@ -1,6 +1,5 @@
 import json
 from typing import TypedDict
-from datetime import date, datetime, timedelta
 
 from SharkBot import Destiny
 
@@ -15,38 +14,34 @@ class _RotationData(TypedDict):
 
 
 class Raid:
+    raids = []
+    rotation = []
+    seasonal = None
 
     def __init__(self, name: str) -> None:
         self.name = name
+
+    @classmethod
+    def get(cls, search: str):
+        for raid in cls.raids:
+            if raid.name == search:
+                return raid
+        else:
+            raise Destiny.Errors.RaidNotFoundError(search)
+
+    @classmethod
+    def get_current(cls):
+        return cls.rotation[Destiny.get_week_index() % len(cls.rotation)]
 
 
 with open("data/static/destiny/raids/raids.json", "r") as infile:
     raidData: list[_RaidData] = json.load(infile)
 
-raids: list[Raid] = [Raid(**data) for data in raidData]
-
-
-def get(search: str) -> Raid:
-    for raid in raids:
-        if raid.name == search:
-            return raid
-    else:
-        raise Destiny.Errors.RaidNotFoundError(search)
+Raid.raids = [Raid(**data) for data in raidData]
 
 
 with open("data/static/destiny/raids/rotation.json", "r") as infile:
     rotationData: _RotationData = json.load(infile)
 
-seasonal: Raid = get(rotationData["seasonal"])
-rotation: list[Raid] = [get(raidName) for raidName in rotationData["featured"]]
-rotationStart = datetime(year=2022, month=8, day=23)
-
-
-def get_current() -> Raid:
-    dtnow = datetime.utcnow()
-    if dtnow.time() < Destiny.reset_time:
-        dtnow = dtnow - timedelta(days=1)
-    days = (dtnow - rotationStart).days
-    weeks = int(days / 7)
-    position = weeks % len(rotation)
-    return rotation[position]
+Raid.seasonal = Raid.get(rotationData["seasonal"])
+Raid.rotation = [Raid.get(raidName) for raidName in rotationData["featured"]]
