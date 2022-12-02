d134039d12a2499479ec9812b1c59bdf54cc7660
James Donald <dev@chaoscantrip.com>
2022-12-02 23:34:22 +0000

    Utilised Utils.td_to_string()

--- SharkBot/Destiny/Season.py
+++ SharkBot/Destiny/Season.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 import json
 
+from SharkBot import Utils
+
 
 class Season:
     current = None
@@ -24,40 +26,7 @@ class Season:
 
     @property
     def time_remaining_string(self) -> str:
-        seconds = int(self.time_remaining.total_seconds())
-        days, seconds = seconds // (24 * 60 * 60), seconds % (24 * 60 * 60)
-        hours, seconds = seconds // (60 * 60), seconds % (60 * 60)
-        minutes, seconds = seconds // 60, seconds % 60
-
-        output_string = ""
-        if days != 0:
-            if days == 1:
-                output_string += f"{days} day, "
-            else:
-                output_string += f"{days} days, "
-        if hours != 0:
-            if hours == 1:
-                output_string += f"{hours} hour, "
-            else:
-                output_string += f"{hours} hours, "
-        if minutes != 0:
-            if minutes == 1:
-                output_string += f"{minutes} minute, "
-            else:
-                output_string += f"{minutes} minutes, "
-        if output_string == "":
-            if seconds == 1:
-                output_string += f"{seconds} second "
-            else:
-                output_string += f"{seconds} seconds "
-        else:
-            output_string = output_string[:-2] + f" and {seconds} "
-            if seconds == 1:
-                output_string += f"second "
-            else:
-                output_string += f"seconds "
-
-        return output_string
+        return Utils.td_to_string(self.time_remaining)
 
 
 with open("data/static/destiny/current_season.json", "r") as infile:
