24e9ae042048de3a8fe54f541b622f7d15b0d890
James Donald <dev@chaoscantrip.com>
2022-12-12 17:23:29 +0000

    Added basis for $redeem

--- cogs/redeem.py
+++ cogs/redeem.py
@@ -7,6 +7,14 @@ class Redeem(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
+    @commands.command()
+    async def redeem(self, ctx: commands.Context, code: str):
+        embed = discord.Embed()
+        embed.title = "Redeem"
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar.url)
+
+        await ctx.reply(embed=embed)
+
 
 async def setup(bot):
     await bot.add_cog(Redeem(bot))
