076da4801e97ad3301ab1aa0ca2f207df245bcf0
James Donald <dev@chaoscantrip.com>
2022-12-17 23:15:17 +0000

    Added folder existence checking to API

--- SharkBot/API.py
+++ SharkBot/API.py
@@ -1,10 +1,21 @@
 import json
+import os
+
 import SharkBot
+
+api_folder_path = "data/live/api"
+
+if not os.path.exists(api_folder_path):  # Ensure api folder exists
+    os.makedirs(api_folder_path)
+
+if not os.path.exists(f"{api_folder_path}/last_counts.json"):
+    with open(f"{api_folder_path}/last_counts.json", "w+") as outfile:
+        json.dump({}, outfile, indent=4)
 
 
 def check_changed_counts() -> list[SharkBot.Member.Member]:
     output = []
-    with open("data/live/api/last_counts.json", "r") as infile:
+    with open(f"{api_folder_path}/last_counts.json", "r") as infile:
         data: dict[int, int] = json.load(infile)
     for member in SharkBot.Member.members.values():
         if member.id not in data.keys():
@@ -17,5 +28,5 @@ def check_changed_counts() -> list[SharkBot.Member.Member]:
 
 def write_counts():
     data = {member.id: member.counts for member in SharkBot.Member.members.values()}
-    with open("data/live/api/last_counts.json", "w+") as outfile:
+    with open(f"{api_folder_path}/last_counts.json", "w+") as outfile:
         json.dump(data, outfile, indent=4)
