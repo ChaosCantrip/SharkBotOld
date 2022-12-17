f9681b435d9e2edbcc54e58bd9e9dfdd61468073
James Donald <dev@chaoscantrip.com>
2022-12-17 23:10:49 +0000

    Added SharkBot.API.check_changed_counts()

--- SharkBot/API.py
+++ SharkBot/API.py
@@ -1,1 +1,15 @@
+import json
 import SharkBot
+
+
+def check_changed_counts() -> list[SharkBot.Member.Member]:
+    output = []
+    with open("data/live/api/last_counts.json", "r") as infile:
+        data: dict[int, int] = json.load(infile)
+    for member in SharkBot.Member.members.values():
+        if member.id not in data.keys():
+            output.append(member)
+        else:
+            if member.counts != data[member.id]:
+                output.append(member)
+    return output
