57b4fe3e18da5699889c5b2ed48b655cd25870cf
James Donald <dev@chaoscantrip.com>
2022-12-17 23:23:42 +0000

    Added apiHandler.upload_counts()

--- SharkBot/Handlers/apiHandler.py
+++ SharkBot/Handlers/apiHandler.py
@@ -1,2 +1,16 @@
 import secret
 import aiohttp
+
+
+async def upload_counts(member_id: int, member_name: str, counts: int):
+    async with aiohttp.ClientSession() as session:
+        async with session.put(
+            url=f"{secret.SharkBotAPI.base_uri}/upload_count",
+            json={
+                "member_id": member_id,
+                "member_name": member_name,
+                "counts": counts
+            },
+            headers=secret.SharkBotAPI.auth_header
+        ) as response:
+            print(response.status)
