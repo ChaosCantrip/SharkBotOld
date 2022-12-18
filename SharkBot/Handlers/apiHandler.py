334ad7de2397a11a7f2a539de79bf000cfa2609c
James Donald <dev@chaoscantrip.com>
2022-12-18 08:25:21 +0000

    Added member uploading

--- SharkBot/Handlers/apiHandler.py
+++ SharkBot/Handlers/apiHandler.py
@@ -1,22 +1,23 @@
+from typing import Union
+
 import secret
 import aiohttp
 
 
-async def upload_counts(data: list[list]):
-    payload = {
-        "members": {
-            str(d[0]): {
-                "member_id": d[0],
-                "member_name": d[1],
-                "counts": d[2]
-            } for d in data
+async def upload_data(data: dict[str, dict[str, Union[str, int]]]):
+    for member_id, member_data in data.items():
+        payload = {
+            "member_id": member_id,
+            "data": {
+                key: value for key, value in member_data.items()
+            }
         }
-    }
-    async with aiohttp.ClientSession() as session:
-        async with session.put(
-            url=f"{secret.SharkBotAPI.base_uri}/upload_multi",
-            json=payload,
-            headers=secret.SharkBotAPI.auth_header
-        ) as response:
-            if response.status != 201:
-                print("Fuck")
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                url=f"{secret.SharkBotAPI.base_uri}/member/edit",
+                json=payload,
+                headers=secret.SharkBotAPI.auth_header
+            ) as response:
+                if response.status != 201:
+                    print("Fuck")
