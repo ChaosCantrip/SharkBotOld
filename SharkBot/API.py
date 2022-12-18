5318a1401fd969652c559070d0667a2f5eb6c97b
James Donald <dev@chaoscantrip.com>
2022-12-18 07:59:44 +0000

    Updated API.check_differences()

--- SharkBot/API.py
+++ SharkBot/API.py
@@ -8,25 +8,29 @@ api_folder_path = "data/live/api"
 if not os.path.exists(api_folder_path):  # Ensure api folder exists
     os.makedirs(api_folder_path)
 
-if not os.path.exists(f"{api_folder_path}/last_counts.json"):
-    with open(f"{api_folder_path}/last_counts.json", "w+") as outfile:
+if not os.path.exists(f"{api_folder_path}/last_upload.json"):
+    with open(f"{api_folder_path}/last_upload.json", "w+") as outfile:
         json.dump({}, outfile, indent=4)
 
 
-def check_changed_counts() -> list[SharkBot.Member.Member]:
-    output = []
-    with open(f"{api_folder_path}/last_counts.json", "r") as infile:
-        data: dict[int, int] = json.load(infile)
+async def check_differences(bot) -> dict[int, dict[str, int]]:
+    output = {}
+    with open(f"{api_folder_path}/last_upload.json", "r") as infile:
+        data: dict[int, dict[str, int]] = json.load(infile)
     for member in SharkBot.Member.members.values():
+        member_data = await member.get_snapshot_data(bot)
         if member.id not in data.keys():
-            output.append(member)
+            output[member.id] = member_data
         else:
-            if member.counts != data[member.id]:
-                output.append(member)
+            if member_data != data[member.id]:
+                output[member.id] = {}
+                for key in ["display_name", "avatar_url", "counts"]:
+                    if member_data[key] != data[member.id][key]:
+                        output[member.id][key] = member_data[key]
     return output
 
 
-def write_counts():
+def write_snapshot():
     data = {member.id: member.counts for member in SharkBot.Member.members.values()}
-    with open(f"{api_folder_path}/last_counts.json", "w+") as outfile:
+    with open(f"{api_folder_path}/last_upload.json", "w+") as outfile:
         json.dump(data, outfile, indent=4)
