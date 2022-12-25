1268d05839c30cf730a6e31f0c1d2ed134817980
ChaosCantrip <james.donald.02@gmail.com>
2022-12-25 19:02:55 +0000

    Added Anniversary Item icon

--- SharkBot/Icons.py
+++ SharkBot/Icons.py
@@ -13,5 +13,6 @@ Collections = {
     "slime_rancher_item": "<:slime_rancher_item:1023831794602229780>",
     "halloween_item": "<:halloween_item:1030596811758514207>",
     "christmas_item": "<:christmas_item:1047569432689528915>",
-    "fragment_item": "<:fragment_item:1051956781359829073>"
+    "fragment_item": "<:fragment_item:1051956781359829073>",
+    "anniversary_item": "<:anniversary_item:1056645481121448007>"
 }
