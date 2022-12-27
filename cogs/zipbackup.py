ff320b779ed2f4d47e99d65610d6dda415a2c27d
ChaosCantrip <james.donald.02@gmail.com>
2022-12-27 19:51:33 +0000

    Added $take_backup

--- cogs/zipbackup.py
+++ cogs/zipbackup.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 import SharkBot
 
@@ -8,6 +9,15 @@ class ZIPBackup(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def take_backup(self, ctx: commands.Context):
+        await ctx.reply("Creating backup...")
+        dt = datetime.now().date()
+        SharkBot.ZIPBackup.create_backup(dt)
+        await SharkBot.ZIPBackup.send_backup(ctx, dt)
+
 
 
 async def setup(bot):
