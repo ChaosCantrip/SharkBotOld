ad6fb08e8f6932a8195fa2ca4370af0fef59f57a
ChaosCantrip <james.donald.02@gmail.com>
2022-12-27 19:38:12 +0000

    Added ZIPBackup.create_backup()

--- SharkBot/ZIPBackup.py
+++ SharkBot/ZIPBackup.py
@@ -1,1 +1,12 @@
+import shutil
+import os
+from datetime import datetime, timedelta, date
+from typing import Optional
+
 from SharkBot.Errors import ZIPBackup as Errors
+
+
+def create_backup(dt: Optional[date]):
+    if dt is None:
+        dt = datetime.now().date()
+    shutil.make_archive(f"data/live/backups/{dt}", "zip", "data/live/members")
