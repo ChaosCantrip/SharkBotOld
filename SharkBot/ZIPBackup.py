4c06b1ae6496de3ba56d08948729f6b27f52ffc2
ChaosCantrip <james.donald.02@gmail.com>
2022-12-27 19:41:39 +0000

    Added ZIPBackup.delete_backup()

--- SharkBot/ZIPBackup.py
+++ SharkBot/ZIPBackup.py
@@ -6,7 +6,16 @@ from typing import Optional
 from SharkBot.Errors import ZIPBackup as Errors
 
 
-def create_backup(dt: Optional[date]):
+def create_backup(dt: Optional[date] = None):
     if dt is None:
         dt = datetime.now().date()
     shutil.make_archive(f"data/live/backups/{dt}", "zip", "data/live/members")
+
+
+def delete_backup(dt: Optional[date] = None):
+    if dt is None:
+        dt = datetime.now().date()
+    if os.path.exists(f"data/live/backups/{dt}.zip"):
+        os.remove(f"data/live/backups/{dt}.zip")
+    else:
+        raise Errors.BackupDoesNotExistError(f"{dt}.zip")
