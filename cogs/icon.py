4503997d43f4c70f2ad0cf544b5d6de265f5def6
ChaosCantrip <james.donald.02@gmail.com>
2022-12-29 15:03:21 +0000

    Added $icon_list

--- cogs/icon.py
+++ cogs/icon.py
@@ -9,6 +9,21 @@ class Icon(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.command()
+    @commands.is_owner()
+    async def icon_list(self, ctx):
+        embed = discord.Embed()
+        embed.title = "Icon List"
+        for name, icon in SharkBot.Icon.icon_dict().items():
+            embed.add_field(
+                name=f"{icon} {name}",
+                value=f"`{icon}`",
+                inline=False
+            )
+
+        for e in SharkBot.Utils.split_embeds(embed):
+            await ctx.send(embed=e)
+
 
 async def setup(bot):
     await bot.add_cog(Icon(bot))
