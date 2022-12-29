3ed28b9350a9cd11d908cde79c6345fc28ff85e5
ChaosCantrip <james.donald.02@gmail.com>
2022-12-29 11:18:30 +0000

    Removed Icons.Collections

--- SharkBot/Icons.py
+++ SharkBot/Icons.py
@@ -1,27 +1,6 @@
 import json
 import os
-
 import discord
-
-Collections = {
-    "witch_queen_item": "<:witch_queen_item:1023837957771251772>",
-    "exotic_item": "<:exotic_item:964988904035983432>",
-    "mythic_item": "<:mythic_item:964988904124084315>",
-    "common_item": "<:common_item:964988904371535902>",
-    "easter_item": "<:easter_item:964988904388321320>",
-    "rare_item": "<:rare_item:964988904421867591>",
-    "legendary_item": "<:legendary_item:964988904459604028>",
-    "uncommon_item": "<:uncommon_item:964988904535105566>",
-    "valentines_item": "<:valentines_item:964996933234073600>",
-    "lootboxes_item": "<:lootboxes_item:964996933351538790>",
-    "summer_item": "<:summer_item:1012587024248754176>",
-    "slime_rancher_item": "<:slime_rancher_item:1023831794602229780>",
-    "halloween_item": "<:halloween_item:1030596811758514207>",
-    "christmas_item": "<:christmas_item:1047569432689528915>",
-    "fragment_item": "<:fragment_item:1051956781359829073>",
-    "new_year_item": "<:new_year_item:1056659303660007465>",
-    "anniversary_item": "<:anniversary_item:1056645481121448007>"
-}
 
 
 class Icons:
