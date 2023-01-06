eeeb03dc78efd866a2c00808a5bf3d8dce6beb1b
James Donald <dev@chaoscantrip.com>
2023-01-06 16:01:01 +0000

    Added MemberVault Constructor

--- SharkBot/MemberVault.py
+++ SharkBot/MemberVault.py
@@ -1,4 +1,7 @@
 import SharkBot
 
 class MemberVault:
-    pass
+
+    def __init__(self, items: list[str], auto: list[str]):
+        self._items: list[SharkBot.Item.Item] = [SharkBot.Item.get(item_id) for item_id in items]
+        self._auto: set[SharkBot.Item.Item] = {SharkBot.Item.get(item_id) for item_id in auto}
