f8ebf4470c6a4e2a86ad1cc6a3fc9ac4610cb06d
James Donald <dev@chaoscantrip.com>
2023-01-06 16:18:47 +0000

    Added __iter__ to _Items and _Auto

--- SharkBot/MemberVault.py
+++ SharkBot/MemberVault.py
@@ -5,10 +5,16 @@ class _Items:
     def __init__(self, items: list[str]):
         self._items: list[SharkBot.Item.Item] = [SharkBot.Item.get(item_id) for item_id in items]
 
+    def __iter__(self):
+        return (item for item in self._items)
+
 class _Auto:
 
     def __init__(self, items: list[str]):
         self._items: set[SharkBot.Item.Item] = {SharkBot.Item.get(item_id) for item_id in items}
+
+    def __iter__(self):
+        return (item for item in self._items)
 
 class MemberVault:
 
