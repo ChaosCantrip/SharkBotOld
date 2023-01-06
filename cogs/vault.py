ed0ab7a9233f0cacc2ea41987eab106851e6e55c
James Donald <dev@chaoscantrip.com>
2023-01-06 16:51:39 +0000

    Added $vault auto command

--- cogs/vault.py
+++ cogs/vault.py
@@ -36,6 +36,33 @@ class Vault(commands.Cog):
         for embed in embeds:
             await ctx.reply(embed=embed, mention_author=False)
 
+    @vault.command(aliases=["a"])
+    async def auto(self, ctx: commands.Context):
+        member = SharkBot.Member.get(ctx.author.id)
+
+        embed = discord.Embed()
+        embed.title = f"{ctx.author.display_name}'s Vault Auto"
+        embed.description = f"{len(member.vault.auto)} items set to auto-vault"
+        embed.set_thumbnail(url=ctx.author.display_avatar.url)
+        embed.url = member.wiki_profile_url
+
+        for collection in SharkBot.Collection.collections:
+            field_text = []
+            for item in collection.items:
+                if item in member.vault.auto:
+                    field_text.append(
+                        f"{item.name} *({item.id})*"
+                    )
+            if len(field_text) > 0:
+                embed.add_field(
+                    name=str(collection),
+                    value="\n".join(field_text)
+                )
+
+        embeds = SharkBot.Utils.split_embeds(embed)
+        for embed in embeds:
+            await ctx.reply(embed=embed, mention_author=False)
+
 
 async def setup(bot):
     await bot.add_cog(Vault(bot))
