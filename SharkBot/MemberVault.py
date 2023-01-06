f9375e8518d73cb003b81fbaf02a312f91e7b6ce
James Donald <dev@chaoscantrip.com>
2023-01-06 16:03:30 +0000

    Added MemberVault.items property

--- SharkBot/MemberVault.py
+++ SharkBot/MemberVault.py
@@ -5,3 +5,8 @@ class MemberVault:
     def __init__(self, items: list[str], auto: list[str]):
         self._items: list[SharkBot.Item.Item] = [SharkBot.Item.get(item_id) for item_id in items]
         self._auto: set[SharkBot.Item.Item] = {SharkBot.Item.get(item_id) for item_id in auto}
+
+    @property
+    def items(self) -> list[SharkBot.Item.Item]:
+        return list(self._items)
+
