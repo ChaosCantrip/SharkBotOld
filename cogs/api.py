e484970c3729ee3ebe0f3184c1ea4aab436cd51c
James Donald <dev@chaoscantrip.com>
2023-01-07 13:09:13 +0000

    Implemented firestoreHandler in Api Cog

--- cogs/api.py
+++ cogs/api.py
@@ -19,44 +19,24 @@ class API(commands.Cog):
     async def update_database(self):
         for member in SharkBot.Member.members.values():
             await member.fetch_discord_user(self.bot)
-        data_to_change = SharkBot.API.check_differences()
+        changed_member_data = SharkBot.API.check_differences()
         SharkBot.API.write_snapshot()
-        if len(data_to_change) > 0:
-            status_code, response_data = await SharkBot.Handlers.apiHandler.upload_data(data_to_change)
-            members_changed = len(data_to_change)
-            records_changed = sum(len(d) for d in data_to_change.values())
+        if len(changed_member_data) > 0:
+            members_changed = len(changed_member_data)
             db_log_channel = await self.bot.fetch_channel(SharkBot.IDs.channels["Database Log"])
             output_embed = discord.Embed()
             output_embed.title = "Database Upload Complete"
-            output_embed.description = f"<t:{int(datetime.now().timestamp())}:D>"
-            output_embed.set_footer(text=f"Status Code: {status_code}")
-            output_embed.add_field(
-                name=f"Updated {records_changed} records for {members_changed} members.",
-                value=f"```json\n{json.dumps(data_to_change, indent=2)}\n```"
-            )
-            output_embed.add_field(
-                name=f"Response",
-                value=f"```json{json.dumps(response_data, indent=2)}```"
-            )
+            output_embed.description = f"<t:{int(datetime.now().timestamp())}:D>\nUpdated {members_changed} members."
+            for member_data in changed_member_data:
+                SharkBot.Handlers.firestoreHandler.update_data(member_data["id"], member_data)
+                output_embed.add_field(
+                    name=member_data["display_name"] + "'s Data",
+                    value=f"```{json.dumps(member_data, indent=2)}```",
+                    inline=False
+                )
             embeds = SharkBot.Utils.split_embeds(output_embed)
-            messages = []
             for embed in embeds:
-                messages.append(await db_log_channel.send(embed=embed))
-
-            if status_code != 201:
-                dev = await self.bot.fetch_user(SharkBot.IDs.dev)
-                embed = discord.Embed()
-                embed.title = "Database Upload Error"
-                embed.description = f"Status Code: {status_code}"
-                embed.add_field(
-                    name="Response Data",
-                    value=f"```json{json.dumps(response_data, indent=2)}```"
-                )
-                embed.add_field(
-                    name="Related Messages",
-                    value="\n".join(message.jump_url for message in messages)
-                )
-                await dev.send(embed=embed)
+                await db_log_channel.send(embed=embed)
 
 
     @update_database.before_loop
@@ -72,9 +52,7 @@ class API(commands.Cog):
     async def force_upload(self, ctx: commands.Context, target: discord.Member):
         member = SharkBot.Member.get(target.id)
         message = await ctx.reply(f"Uploading data for {target.mention}...")
-        await SharkBot.Handlers.apiHandler.upload_data({
-            str(member.id): member.snapshot_data
-        })
+        await SharkBot.Handlers.firestoreHandler.update_data(member.id, member.snapshot_data)
         await message.edit(content=message.content + " Done.")
 
     @commands.command()
@@ -84,9 +62,7 @@ class API(commands.Cog):
         message = await ctx.reply(f"Uploading all member data... (0/{num})")
         for i, member in enumerate(SharkBot.Member.members.values()):
             await message.edit(content=f"Uploading all member data... ({i+1}/{num})\n`{member.id}`")
-            await SharkBot.Handlers.apiHandler.upload_data({
-                str(member.id): member.snapshot_data
-            })
+            await SharkBot.Handlers.firestoreHandler.update_data(member.id, member.snapshot_data)
         await message.edit(content=f"Uploaded all member data for {num} members.")
 
 
