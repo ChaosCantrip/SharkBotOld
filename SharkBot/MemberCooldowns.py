cc71763a47e924f1627db135682f092f7ab427fd
James Donald <dev@chaoscantrip.com>
2023-01-09 16:04:24 +0000

    Added MemberCooldowns.__init__

--- SharkBot/MemberCooldowns.py
+++ SharkBot/MemberCooldowns.py
@@ -1,2 +1,10 @@
+from datetime import timedelta
+from typing import Optional
+from .Cooldown import Cooldown
+
 class MemberCooldowns:
-    pass
+
+    def __init__(self, hourly: Optional[str] = None, daily: Optional[str] = None, weekly: Optional[str] = None):
+        self.hourly = Cooldown(name="hourly", duration=timedelta(hours=1), expiry=hourly)
+        self.daily = Cooldown(name="daily", duration=timedelta(days=1), expiry=daily)
+        self.weekly = Cooldown(name="weekly", duration=timedelta(weeks=1), expiry=weekly)
