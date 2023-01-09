e06682740771a57d772317364a92346a0f05ffe0
James Donald <dev@chaoscantrip.com>
2023-01-09 18:09:48 +0000

    Added MemberSnapshot.__init__

--- SharkBot/MemberSnapshot.py
+++ SharkBot/MemberSnapshot.py
@@ -3,6 +3,9 @@ import SharkBot
 _SNAPSHOTS_DIRECTORY = "data/live/snapshots/members"
 
 class MemberSnapshot:
-    pass
+
+    def __init__(self, member):
+        self.member: SharkBot.Member.Member = member
+        self.path: str = f"{_SNAPSHOTS_DIRECTORY}/{member.id}.json"
 
 SharkBot.Utils.FileChecker.directory(_SNAPSHOTS_DIRECTORY)
