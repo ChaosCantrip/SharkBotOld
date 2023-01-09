26948c07f061c323b60d7780cb368c615e508a08
James Donald <dev@chaoscantrip.com>
2023-01-09 11:27:01 +0000

    Moved Versions into MemberDataVersions

--- SharkBot/MemberDataConverter.py
+++ SharkBot/MemberDataConverter.py
@@ -1,43 +1,11 @@
 import json
-from typing import Self
-
-class _VERSION:
-
-    @classmethod
-    def _get_version(cls) -> int:
-        return int(cls.__name__[-1])
-
-    @classmethod
-    def _get_last_version(cls) -> Self:
-        return versions[cls._get_version() - 2]
-
-    @classmethod
-    def convert(cls, member_data: dict) -> dict:
-        if member_data["data_version"] != cls._get_version() - 1:
-            member_data = cls._get_last_version().convert(member_data)
-        member_data["data_version"] = cls._get_version()
-        return cls._convert(member_data)
-
-    @staticmethod
-    def _convert(member_data: dict) -> dict:
-        return member_data
-
-
-class _Version1(_VERSION):
-
-    @staticmethod
-    def _convert(member_data: dict) -> dict:
-        return member_data
-
-versions = [
-    _Version1
-]
+from .MemberDataVersions import *
 
 
 class MemberDataConverter:
 
     @staticmethod
-    def _get_latest_version() -> _VERSION:
+    def _get_latest_version() -> VERSION:
         return versions[-1]
 
     @classmethod
