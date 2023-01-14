41dbb01ad7ebd76387f47b93d64c441b921c44e0
James Donald <dev@chaoscantrip.com>
2023-01-14 19:26:49 +0000

    Added $effects command

--- cogs/effects.py
+++ cogs/effects.py
@@ -1,11 +1,36 @@
 import discord
 from discord.ext import tasks, commands
 
+import SharkBot
 
 class Effects(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+
+    @commands.command()
+    async def effects(self, ctx: commands.Context):
+        member = SharkBot.Member.get(ctx.author.id)
+
+        embed = discord.Embed()
+        embed.title = f"{ctx.author.display_name}'s Effects"
+        embed.set_thumbnail(
+            url=ctx.author.display_avatar.url
+        )
+        effects = member.effects.details
+        if len(effects) > 0:
+            embed.description = f"You have `{len(effects)}` effects active."
+            for effect in effects:
+                embed.add_field(
+                    name=effect[0],
+                    value=effect[1],
+                    inline=False
+                )
+        else:
+            embed.description = "You have no active effects."
+
+        for e in SharkBot.Utils.split_embeds(embed):
+            await ctx.reply(embed=e, mention_author=False)
 
 
 async def setup(bot):
