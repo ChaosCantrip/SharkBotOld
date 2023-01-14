f69a6d173e53ad74bc59f0b290835243ba1f26d0
James Donald <dev@chaoscantrip.com>
2023-01-14 17:56:02 +0000

    Added Clover and Die to InventoryAddResponse

--- SharkBot/Response/InventoryAddResponse.py
+++ SharkBot/Response/InventoryAddResponse.py
@@ -3,10 +3,12 @@ import SharkBot
 
 class InventoryAddResponse:
 
-    def __init__(self, item: SharkBot.Item.Item = None, new_item: bool = False, auto_vault: bool = False):
+    def __init__(self, item: SharkBot.Item.Item = None, new_item: bool = False, auto_vault: bool = False, clover_used: bool = False, dice_used: bool = False):
         self.item = item
         self.new_item = new_item
         self.auto_vault = auto_vault
+        self.clover_used = clover_used
+        self.dice_used = dice_used
 
     @property
     def flags(self) -> list[str]:
@@ -15,6 +17,10 @@ class InventoryAddResponse:
             _flags.append(":sparkles:")
         if self.auto_vault:
             _flags.append(":gear:")
+        if self.clover_used:
+            _flags.append(":four_leaf_clover:")
+        if self.dice_used:
+            _flags.append(":game_die:")
         return _flags
 
     @property
