1c84fcc7f6352899897e9e5b7103ed26ca3fe9cc
James Donald <dev@chaoscantrip.com>
2023-01-18 05:28:10 +0000

    Added MemberBungie token attributes

--- SharkBot/MemberBungie.py
+++ SharkBot/MemberBungie.py
@@ -1,4 +1,11 @@
+from datetime import datetime
+from typing import Optional
+
+
 class MemberBungie:
 
     def __init__(self, member):
-        self.member = member
+        self._member = member
+        self._token: Optional[str] = None
+        self._token_expires: Optional[datetime] = None
+        self._refresh_token_expires: Optional[datetime] = None
