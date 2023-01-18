44f0aff34769598fa57634d6a691ca731602728a
James Donald <dev@chaoscantrip.com>
2023-01-18 05:39:19 +0000

    Added MemberBungie._refresh_token()

--- SharkBot/MemberBungie.py
+++ SharkBot/MemberBungie.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 from typing import Optional
+import aiohttp
+import secret
+from SharkBot import Errors
 
 
 class MemberBungie:
@@ -9,3 +12,11 @@ class MemberBungie:
         self._token: Optional[str] = None
         self._token_expires: Optional[datetime] = None
         self._refresh_token_expires: Optional[datetime] = None
+
+    async def _refresh_token(self) -> bool:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(secret.BungieAPI.REFRESH_URL, data=secret.BungieAPI.refresh_headers(self._member.id)) as response:
+                if response.ok:
+                    return True
+                else:
+                    return False
