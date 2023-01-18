bb45e328006abc287c1a20ad07d09c75bf04d4af
James Donald <dev@chaoscantrip.com>
2023-01-18 05:21:18 +0000

    Enabled always firestore

--- SharkBot/Handlers/firestoreHandler.py
+++ SharkBot/Handlers/firestoreHandler.py
@@ -4,13 +4,10 @@ from firebase_admin import firestore
 import secret
 import colorama
 
-if not secret.testBot:
-    _cred = credentials.Certificate("firebase.sbignore.json")
-    _app = firebase_admin.initialize_app(_cred)
-    db = firestore.client()
-    print(colorama.Fore.GREEN + colorama.Style.BRIGHT + "Firestore Client Initialised")
-else:
-    print(colorama.Fore.YELLOW + colorama.Style.BRIGHT + "Firestore Client Ignored (Test Bot)")
+_cred = credentials.Certificate("firebase.sbignore.json")
+_app = firebase_admin.initialize_app(_cred)
+db = firestore.client()
+print(colorama.Fore.GREEN + colorama.Style.BRIGHT + "Firestore Client Initialised")
 
 def upload_data(member_data: dict):
     if secret.testBot:
