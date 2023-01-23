cd438f43fcf0577ca9b2d07e7e1559892000cab7
James Donald <dev@chaoscantrip.com>
2023-01-23 22:14:22 +0000

    Added EventCalendar.get_current()

--- SharkBot/EventCalendar.py
+++ SharkBot/EventCalendar.py
@@ -1,4 +1,4 @@
-from typing import Self
+from typing import Self, Optional
 from datetime import datetime, timedelta
 from SharkBot import Item
 
@@ -11,3 +11,12 @@ class EventCalendar:
         self.start_date = datetime.strptime(start_date, _TIME_FORMAT).date()
         self.end_date = self.start_date + timedelta(days=len(item_list))
         self.items = [Item.get(item_id) for item_id in item_list]
+
+    @classmethod
+    def get_current(cls) -> Optional[Self]:
+        current_date = datetime.now().date()
+        for event_calendar in cls.event_calendars:
+            if event_calendar.start_date < current_date < event_calendar.end_date:
+                return event_calendar
+        else:
+            return None
