3be3aa8cef95c03b5c6d8533a02347907b4689fd
James Donald <dev@chaoscantrip.com>
2023-01-23 22:11:33 +0000

    Added EventCalendar.__init__

--- SharkBot/EventCalendar.py
+++ SharkBot/EventCalendar.py
@@ -1,4 +1,13 @@
 from typing import Self
+from datetime import datetime, timedelta
+from SharkBot import Item
+
+_TIME_FORMAT = "%d/%m/%Y"
 
 class EventCalendar:
     event_calendars: list[Self] = []
+
+    def __init__(self, start_date: str, item_list: list[str]):
+        self.start_date = datetime.strptime(start_date, _TIME_FORMAT).date()
+        self.end_date = self.start_date + timedelta(days=len(item_list))
+        self.items = [Item.get(item_id) for item_id in item_list]
