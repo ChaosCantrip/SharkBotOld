3015160d93c72bcc041b524fb7fb8dd0a337be45
James Donald <dev@chaoscantrip.com>
2023-02-01 08:56:30 +0000

    Added $leaderboard command

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -21,6 +21,24 @@ class Leaderboard(commands.Cog):
                 leaderboard.upload()
                 leaderboard.save_snapshot(snapshot)
 
+    @commands.command()
+    async def leaderboard(self, ctx: commands.Context, *, lb: SharkBot.Leaderboard.Leaderboard):
+        member = SharkBot.Member.get(ctx.author.id)
+        lb_snapshot = lb.create_ranked()
+
+        embed = discord.Embed()
+        embed.title = f"{lb.name} Leaderboard"
+        content = []
+        for lb_member in lb_snapshot:
+            if lb_member.member.discord_user is None:
+                await lb_member.member.fetch_discord_user(self.bot)
+            content.append(f"{lb_member.rank}. {lb_member.member_display_name} - {lb_member.value}")
+            if lb_member.member == member:
+                content[-1] = f"**{content[-1]}**"
+        embed.description = "\n".join(content)
+        await ctx.reply(embed=embed, mention_author=False)
+
+
 
 async def setup(bot):
     await bot.add_cog(Leaderboard(bot))
