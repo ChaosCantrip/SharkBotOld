91fd21aa73eb6f0ac963b04fe85ba5922254f795
James Donald <dev@chaoscantrip.com>
2023-02-01 06:27:46 +0000

    Added Leaderboard __init__

--- SharkBot/Leaderboard.py
+++ SharkBot/Leaderboard.py
@@ -1,2 +1,8 @@
+from typing import Callable, Union
+import SharkBot
+
 class Leaderboard:
-    pass
+
+    def __init__(self, name: str, method: Callable[[SharkBot.Member.Member], Union[int, float]]):
+        self.name = name
+        self.method = method
