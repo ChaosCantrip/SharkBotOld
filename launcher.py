ccb0cd214cd417ced6d9a65de3620ee76f7c6bcc
James Donald <dev@chaoscantrip.com>
2023-02-01 12:34:40 +0000

    Fixed maintenance mode

--- launcher.py
+++ launcher.py
@@ -7,9 +7,9 @@ while True:
     os.system("sudo git pull")
     os.system("sudo python3 main.py")
 
-    if not os.path.exists("instant_restart"):
-        time.sleep(300)
-    elif os.path.exists("maintenance"):
+    if os.path.exists("maintenance"):
         os.system("sudo python3 maintenance.py")
+    elif not os.path.exists("instant_restart"):
+        time.sleep(300)
     else:
         os.remove("instant_restart")
