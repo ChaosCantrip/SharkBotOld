b421a4a79ffe0c56b1aee312d7e8f2181b4fce4f
James Donald <dev@chaoscantrip.com>
2023-02-01 06:39:33 +0000

    Added Leaderboard.get() classmethod

--- SharkBot/Leaderboard.py
+++ SharkBot/Leaderboard.py
@@ -1,5 +1,5 @@
 import json
-from typing import Callable, Union, Optional
+from typing import Callable, Union, Optional, Self
 
 import SharkBot
 
@@ -7,12 +7,20 @@ _SNAPSHOTS_DICT = "data/live/snapshots/leaderboards"
 SharkBot.Utils.FileChecker.directory(_SNAPSHOTS_DICT)
 
 class Leaderboard:
+    _leaderboards_dict: dict[str, Self] = {}
 
     def __init__(self, name: str, method: Callable[[SharkBot.Member.Member], Union[int, float]]):
         self.name = name
         self.method = method
         self.doc_name = "_".join(self.name.lower().split(" "))
         self.save_file = _SNAPSHOTS_DICT + self.doc_name + ".json"
+
+    @classmethod
+    def get(cls, search: str) -> Self:
+        try:
+            return cls._leaderboards_dict[search.lower()]
+        except KeyError:
+            raise SharkBot.Errors.LeaderboardNotFoundError(search)
 
     def create_current(self) -> dict[str, Union[int, float]]:
         return {str(member.id): self.method(member) for member in SharkBot.Member.members}
