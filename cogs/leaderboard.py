c3e03b8c6b4209e72b26e29003e7d7bba1a88e0a
James Donald <dev@chaoscantrip.com>
2023-02-01 08:41:28 +0000

    Added Leaderboard upload_loop

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -8,6 +8,18 @@ class Leaderboard(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.upload_loop.start()
+
+    def cog_unload(self) -> None:
+        self.upload_loop.cancel()
+
+    @tasks.loop(seconds=5)
+    async def upload_loop(self):
+        for leaderboard in SharkBot.Leaderboard.Leaderboard.leaderboards:
+            snapshot = leaderboard.create_current()
+            if leaderboard.has_changed(snapshot):
+                leaderboard.upload()
+                leaderboard.save_snapshot(snapshot)
 
 
 async def setup(bot):
