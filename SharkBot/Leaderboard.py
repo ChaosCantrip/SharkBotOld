0bdaca868932eb7c907836cf637ce93e4fc023da
James Donald <dev@chaoscantrip.com>
2023-02-01 06:30:09 +0000

    Added Leaderboard.create_current()

--- SharkBot/Leaderboard.py
+++ SharkBot/Leaderboard.py
@@ -6,3 +6,6 @@ class Leaderboard:
     def __init__(self, name: str, method: Callable[[SharkBot.Member.Member], Union[int, float]]):
         self.name = name
         self.method = method
+
+    def create_current(self) -> dict[str, Union[int, float]]:
+        return {str(member.id): self.method(member) for member in SharkBot.Member.members}
