05c76cdb25f28434dd558dea5fca2fd08581a502
James Donald <dev@chaoscantrip.com>
2023-02-03 15:06:31 +0000

    Added $get_log command

--- cogs/logger.py
+++ cogs/logger.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import tasks, commands
 import logging
 
+import SharkBot
+
 command_logger = logging.getLogger("command")
 
 class Logger(commands.Cog):
@@ -13,6 +15,11 @@ class Logger(commands.Cog):
     async def on_command(self, ctx: commands.Context):
         command_logger.info(f"{ctx.author.id} {ctx.author.display_name} - ${ctx.command.name} ({ctx.message.content})")
 
+    @commands.command()
+    async def get_log(self, ctx: commands.Context):
+        for filepath in SharkBot.Utils.get_dir_filepaths("data/live/bot/logs"):
+            file = discord.File(filepath)
+            await ctx.reply(f"Log - {file.filename}", file=file, mention_author=False)
 
 async def setup(bot):
     await bot.add_cog(Logger(bot))
