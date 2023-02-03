e3ad15feaf9ee9b8da09a2404261808434008172
James Donald <dev@chaoscantrip.com>
2023-02-03 08:56:25 +0000

    Added ComponentTypeEnum.enum_dict and .enum_list

--- SharkBot/Destiny/ComponentTypeEnum.py
+++ SharkBot/Destiny/ComponentTypeEnum.py
@@ -1,4 +1,9 @@
+from typing import Self
+
+
 class ComponentTypeEnum:
+    enum_dict: dict[int, Self] = {}
+    enum_list: list[Self] = []
 
     def __init__(self, name: str, enum: int, description: str):
         self.name = name
