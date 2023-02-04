b2707746ca972db8c423e9444aa31f4dcfeccb00
James Donald <dev@chaoscantrip.com>
2023-02-04 22:07:52 +0000

    Added MemberSettings.data

--- SharkBot/MemberSettings.py
+++ SharkBot/MemberSettings.py
@@ -2,3 +2,9 @@ class MemberSettings:
 
     def __init__(self, delete_incorrect_counts: bool = False):
         self.delete_incorrect_counts = delete_incorrect_counts
+
+    @property
+    def data(self) -> dict[str, bool]:
+        return {
+            "delete_incorrect_counts": self.delete_incorrect_counts
+        }
