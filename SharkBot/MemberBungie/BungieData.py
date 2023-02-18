706ac8831cf61d81ef5fb7a97a6486e02430b14e
James Donald <dev@chaoscantrip.com>
2023-02-18 02:16:07 +0000

    Added BungieData constructor

--- SharkBot/MemberBungie/BungieData.py
+++ SharkBot/MemberBungie/BungieData.py
@@ -4,6 +4,9 @@ _PARENT_CACHE_FOLDER = "data/live/bungie/cache"
 
 class BungieData:
 
+    def __init__(self, member):
+        self.member: SharkBot.Member.Member = member
+
     @classmethod
     def _cache_folder_path(cls) -> str:
         return f"{_PARENT_CACHE_FOLDER}/{cls.__name__.lower()}"
