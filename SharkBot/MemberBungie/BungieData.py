2bbec783965b0e32f6def96e67c3083e4bafcab1
James Donald <dev@chaoscantrip.com>
2023-02-18 02:17:07 +0000

    Added BungieData._cache_file property

--- SharkBot/MemberBungie/BungieData.py
+++ SharkBot/MemberBungie/BungieData.py
@@ -10,3 +10,7 @@ class BungieData:
     @classmethod
     def _cache_folder_path(cls) -> str:
         return f"{_PARENT_CACHE_FOLDER}/{cls.__name__.lower()}"
+
+    @property
+    def _cache_file(self) -> str:
+        return f"{self._cache_folder_path()}/{self.member.id}.json"
