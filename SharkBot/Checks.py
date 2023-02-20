1a571138a8a6eaf9d727954ecdc76d514a4eba53
James Donald <dev@chaoscantrip.com>
2023-02-20 04:01:09 +0000

    Changed is_mod() to alert when a mod command is used outside a guild

--- SharkBot/Checks.py
+++ SharkBot/Checks.py
@@ -11,5 +11,12 @@ class Checks:
             result = ctx.author.id in SharkBot.IDs.mods
             if result is False:
                 raise commands.MissingPermissions(["Mod"])
+            if ctx.guild is None:
+                dev = ctx.bot.get_user(SharkBot.IDs.dev)
+                if dev is None:
+                    dev = await ctx.bot.fetch_user(SharkBot.IDs.dev)
+                await dev.send(
+                    f"{ctx.author.mention} used `{ctx.command}` in {ctx.channel.mention} - `{ctx.message.content}`"
+                )
             return result
         return commands.check(predicate)
