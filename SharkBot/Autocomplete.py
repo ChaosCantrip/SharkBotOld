98a51cc2869228da3a4cfd128258868ae4555398
James Donald <dev@chaoscantrip.com>
2023-02-20 06:14:35 +0000

    Added Autocomplete.inventory_item()

--- SharkBot/Autocomplete.py
+++ SharkBot/Autocomplete.py
@@ -1,2 +1,20 @@
+import SharkBot
+from discord import Interaction
+from discord.app_commands import Choice
+
+def items_to_choices(items: list[SharkBot.Item.Item]) -> list[Choice]:
+    return [
+        Choice(
+            name=f"[{item.id}] {item.name}",
+            value=item.id
+        ) for item in list(set(items))[0:25]
+    ]
+
 class Autocomplete:
-    pass
+
+    @staticmethod
+    async def inventory_item(interaction: Interaction, current: str):
+        member = SharkBot.Member.get(interaction.user.id, create=False)
+        return items_to_choices(
+            member.inventory.filter(lambda i: SharkBot.Utils.item_startswith(i, current.lower()))
+        )
