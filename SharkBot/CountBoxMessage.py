1f6a2d2d8d1749ff724c8b46b6f52d46a5a93667
James Donald <dev@chaoscantrip.com>
2023-02-20 07:28:28 +0000

    Added file for CountBoxMessage

--- SharkBot/CountBoxMessage.py
+++ SharkBot/CountBoxMessage.py
@@ -1,2 +1,13 @@
+import SharkBot
+
+_MESSAGES_FILEPATH = "data/live/bot/CountBoxMessages.json"
+
+SharkBot.Utils.FileChecker.json(
+    _MESSAGES_FILEPATH,
+    default_value={
+        "886721016766799882": "Hey, would you look at that! You found an [ITEM]"
+    }
+)
+
 class CountBoxMessage:
-    pass
+    messages: dict[str, str] = SharkBot.Utils.JSON.load(_MESSAGES_FILEPATH)
