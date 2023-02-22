4c783e0e6214af1785e1086530b923747747032e
James Donald <dev@chaoscantrip.com>
2023-02-22 23:26:18 +0000

    Added test database

--- SharkBot/Handlers/firestoreHandler.py
+++ SharkBot/Handlers/firestoreHandler.py
@@ -1,12 +1,17 @@
+import os.path
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+
+import SharkBot.Utils
 import secret
 import colorama
 import logging
 
 db_logger = logging.getLogger("firestore")
 
+_FAKE_DB_FILEPATH = "data/live/fake_firestore"
 _cred = credentials.Certificate("firebase.sbignore.json")
 _app = firebase_admin.initialize_app(_cred)
 db = firestore.client()
@@ -22,7 +27,8 @@ def upload_member_data(member_data: dict):
 def set_doc(collection: str, document: str, data: dict) -> None:
     db_logger.info(f"{collection}/{document} - Firestore upload")
     if secret.testBot:
-        return
+        SharkBot.Utils.FileChecker.directory(f"{_FAKE_DB_FILEPATH}/{collection}")
+        SharkBot.Utils.JSON.dump(f"{_FAKE_DB_FILEPATH}/{collection}/{document}.json", data)
     col_ref = db.collection(collection)
     doc_ref = col_ref.document(document)
     doc_ref.set(data)
@@ -30,7 +36,8 @@ def set_doc(collection: str, document: str, data: dict) -> None:
 def del_doc(collection: str, document: str) -> None:
     db_logger.warning(f"{collection}/{document} - Firestore Delete")
     if secret.testBot:
-        return
+        if os.path.isfile(f"{_FAKE_DB_FILEPATH}/{collection}/{document}.json"):
+            os.remove(f"{_FAKE_DB_FILEPATH}/{collection}/{document}.json")
     col_ref = db.collection(collection)
     doc_ref = col_ref.document(document)
     doc_ref.delete()
