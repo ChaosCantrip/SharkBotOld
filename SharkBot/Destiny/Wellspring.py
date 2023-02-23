8022fea2a706614a74d6edab759f56de594a1616
James Donald <dev@chaoscantrip.com>
2023-02-23 13:29:05 +0000

    Added WellspringData dataclass

--- SharkBot/Destiny/Wellspring.py
+++ SharkBot/Destiny/Wellspring.py
@@ -16,6 +16,15 @@ class WellspringDetails:
     def __post_init__(self):
         self.weapon = WellspringWeapon(**self.weapon)
 
+@dataclass
+class WellspringData:
+    rotation_start: dict[str, int]
+    rotation: list[WellspringDetails]
+
+    def __post_init__(self):
+        wellspring_details: dict[str, str | dict[str, str]]
+        self.rotation = [WellspringDetails(**wellspring_details) for wellspring_details in self.rotation]
+
 
 ROTATION_OFFSET = (datetime.date(2023, 1, 1) - SharkBot.Destiny.season_start).days
 
