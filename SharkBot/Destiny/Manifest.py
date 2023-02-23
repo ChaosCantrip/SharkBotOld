40144b9a2fdf4aea5e78ec25ac7e944cfd643e74
James Donald <dev@chaoscantrip.com>
2023-02-23 19:35:57 +0000

    Added _MANIFEST_FOLDER

---
+++ SharkBot/Destiny/Manifest.py
@@ -0,0 +1,5 @@
+from SharkBot import Utils as _Utils
+
+_MANIFEST_FOLDER = "data/live/bungie/manifest"
+_DEFINITIONS_FOLDER = f"{_MANIFEST_FOLDER}/definitions"
+_Utils.FileChecker.directory(_DEFINITIONS_FOLDER)
