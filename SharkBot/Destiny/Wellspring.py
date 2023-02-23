714b3cbbe5d5b8458282fe8d7bd7d787144a6640
James Donald <dev@chaoscantrip.com>
2023-02-23 13:17:51 +0000

    Added WellspringDetails dataclass

--- SharkBot/Destiny/Wellspring.py
+++ SharkBot/Destiny/Wellspring.py
@@ -5,5 +5,11 @@ class WellspringWeapon:
     name: str
     type: str
 
+@dataclass(frozen=True)
+class WellspringDetails:
+    mode: str
+    boss: str
+    weapon: WellspringWeapon
+
 class Wellspring:
     pass
