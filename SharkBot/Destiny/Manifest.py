5ea1d3421206d10aaf5e01dbaebfffa8a1799f3d
James Donald <dev@chaoscantrip.com>
2023-02-23 23:14:00 +0000

    Added Manifest.get_current_manifest()

--- SharkBot/Destiny/Manifest.py
+++ SharkBot/Destiny/Manifest.py
@@ -1,5 +1,14 @@
-from SharkBot import Utils as _Utils
+import os.path
+
+import SharkBot as _SharkBot
 
 _MANIFEST_FOLDER = "data/live/bungie/manifest"
 _DEFINITIONS_FOLDER = f"{_MANIFEST_FOLDER}/definitions"
-_Utils.FileChecker.directory(_DEFINITIONS_FOLDER)
+_MANIFEST_FILE = f"{_MANIFEST_FOLDER}/Manifest.json"
+_SharkBot.Utils.FileChecker.directory(_DEFINITIONS_FOLDER)
+
+def get_current_manifest() -> dict:
+    if os.path.isfile(_MANIFEST_FILE):
+        return _SharkBot.Utils.JSON.load(_MANIFEST_FILE)
+    else:
+        raise _SharkBot.Destiny.Errors.Manifest.ManifestNotFoundError
