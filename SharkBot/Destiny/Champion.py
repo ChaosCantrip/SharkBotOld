9031a5acb712f3aeba6d2db4eb12e9cf48ef8674
James Donald <dev@chaoscantrip.com>
2023-02-27 04:49:02 +0000

    Added Champion.__repr__

--- SharkBot/Destiny/Champion.py
+++ SharkBot/Destiny/Champion.py
@@ -13,6 +13,9 @@ class Champion:
     def __init__(self, name: str) -> None:
         self.name = name
 
+    def __repr__(self):
+        return f"Champion[{self.name}]"
+
     @property
     def icon(self) -> str:
         return Icon.get(f"{self.name.lower()}_icon")
