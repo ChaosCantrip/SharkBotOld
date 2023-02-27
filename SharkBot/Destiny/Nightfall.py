520c3d34e3e536c571d3d09e64912ed2a619cc02
James Donald <dev@chaoscantrip.com>
2023-02-27 05:52:09 +0000

    Added light_level and destination_hash to NightfallDifficulty

--- SharkBot/Destiny/Nightfall.py
+++ SharkBot/Destiny/Nightfall.py
@@ -3,6 +3,8 @@ class NightfallDifficulty:
     def __init__(self, activity_data: dict):
         self.name = activity_data["description"]
         self.difficulty = activity_data["name"]
+        self.light_level: int = activity_data["activityLightLevel"]
+        self.destination_hash: int = activity_data["destinationHash"]
         self.modifier_hashes: list[int] = [m["activityModifierHash"] for m in activity_data["modifiers"]]
 
 class Nightfall:
