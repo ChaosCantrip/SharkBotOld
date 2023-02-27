089045df3f86abe99f104b7df1923e2d4fc2573c
James Donald <dev@chaoscantrip.com>
2023-02-27 05:38:12 +0000

    Added name and difficulty to NightfallDifficulty

--- SharkBot/Destiny/Nightfall.py
+++ SharkBot/Destiny/Nightfall.py
@@ -1,6 +1,8 @@
 class NightfallDifficulty:
 
     def __init__(self, activity_data: dict):
+        self.name = activity_data["description"]
+        self.difficulty = activity_data["name"]
         self.modifier_hashes: list[int] = [m["activityModifierHash"] for m in activity_data["modifiers"]]
 
 class Nightfall:
