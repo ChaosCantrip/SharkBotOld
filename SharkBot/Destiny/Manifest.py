05b286a30dc5e66996c54d55c94f87481ebc43d0
James Donald <dev@chaoscantrip.com>
2023-02-27 09:39:38 +0000

    Added Manifest._hash_to_id() and get_definition()

--- SharkBot/Destiny/Manifest.py
+++ SharkBot/Destiny/Manifest.py
@@ -1,4 +1,7 @@
 import sqlite3
+import json
+
+from SharkBot import Errors
 
 _MANIFEST_FOLDER = "data/live/bungie/manifest"
 _CONTENT_FILE = _MANIFEST_FOLDER + "/manifest.content"
@@ -15,3 +18,23 @@ def _execute(statement: str, fetch_all: bool = True):
     return res
 
 DEFINITION_TYPES = [r[0] for r in _execute("SELECT name FROM sqlite_master WHERE type='table';")]
+
+_HASH_THRESHOLD = 2**31 - 1
+_HASH_MODIFIER = 2**32
+
+def _hash_to_id(hash_in: str | int) -> int:
+    hash_in = int(hash_in)
+    if hash_in > _HASH_THRESHOLD:
+        return hash_in - _HASH_MODIFIER
+    else:
+        return hash_in
+
+def get_definition(definition_type: str, definition_hash: str | int):
+    if definition_type not in DEFINITION_TYPES:
+        raise Errors.Manifest.DefinitionTypeDoesNotExistError(definition_type)
+    definition_id = _hash_to_id(definition_hash)
+    result = _execute(f"SELECT * FROM {definition_type} WHERE id={definition_id}", fetch_all=False)
+    if result is None:
+        raise Errors.Manifest.HashNotFoundError(definition_type, definition_hash, definition_id)
+    else:
+        return json.loads(result[1])
