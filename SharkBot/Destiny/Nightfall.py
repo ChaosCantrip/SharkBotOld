35303aa84e3805a3d309a5e37ac5c523ce493a66
James Donald <dev@chaoscantrip.com>
2023-02-27 05:53:34 +0000

    Added shield_types and champion_types to NightfallDifficulty

--- SharkBot/Destiny/Nightfall.py
+++ SharkBot/Destiny/Nightfall.py
@@ -1,3 +1,5 @@
+from SharkBot.Destiny import Shield, Champion
+
 class NightfallDifficulty:
 
     def __init__(self, activity_data: dict):
@@ -6,6 +8,8 @@ class NightfallDifficulty:
         self.light_level: int = activity_data["activityLightLevel"]
         self.destination_hash: int = activity_data["destinationHash"]
         self.modifier_hashes: list[int] = [m["activityModifierHash"] for m in activity_data["modifiers"]]
+        self.shield_types = Shield.from_modifiers(self.modifier_hashes)
+        self.champion_types = Champion.from_modifiers(self.modifier_hashes)
 
 class Nightfall:
     pass
