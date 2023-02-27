c10a4d34c93e7c5e503058ba20fede0351e004b0
James Donald <dev@chaoscantrip.com>
2023-02-27 11:09:50 +0000

    Added Manifest._update_manifest_blocking()

--- SharkBot/Destiny/Manifest.py
+++ SharkBot/Destiny/Manifest.py
@@ -1,7 +1,9 @@
+import os.path
 import sqlite3
 import json
 import requests
 import aiohttp
+import zipfile
 
 from SharkBot import Errors, Utils
 
@@ -9,6 +11,7 @@ from SharkBot import Errors, Utils
 
 _MANIFEST_FOLDER = "data/live/bungie/manifest"
 _MANIFEST_FILE = "data/live/bungie/manifest/manifest.json"
+_ZIP_TARGET = _MANIFEST_FOLDER + "/manifest.zip"
 _CONTENT_FILE = _MANIFEST_FOLDER + "/manifest.content"
 _BASE_URL = "https://bungie.net"
 _MANIFEST_URL = _BASE_URL + "/Platform/Destiny2/Manifest"
@@ -16,6 +19,7 @@ _MANIFEST_URL = _BASE_URL + "/Platform/Destiny2/Manifest"
 _HASH_THRESHOLD = 2**31 - 1
 _HASH_MODIFIER = 2**32
 
+Utils.FileChecker.directory(_MANIFEST_FOLDER)
 
 # Update Checking
 
@@ -55,6 +59,24 @@ async def is_outdated() -> bool:
         return True
     new_manifest = await _fetch_manifest_async()
     return current_manifest["Response"]["version"] != new_manifest["Response"]["version"]
+
+def _update_manifest_blocking():
+    new_manifest = _fetch_manifest_blocking()
+    Utils.JSON.dump(_MANIFEST_FILE, new_manifest)
+    content_url = _BASE_URL + new_manifest["Response"]["mobileWorldContentPaths"]["en"]
+    response = requests.get(content_url)
+    if not response.ok:
+        raise Errors.Manifest.FetchFailedError("mobileWorldContentPaths", response.status_code)
+    else:
+        with open(_ZIP_TARGET, "wb") as _outfile:
+            _outfile.write(response.content)
+        if os.path.isfile(_CONTENT_FILE):
+            os.remove(_CONTENT_FILE)
+        with zipfile.ZipFile(_ZIP_TARGET) as _zipfile:
+            filename = _MANIFEST_FOLDER + "/" + _zipfile.namelist()[0]
+            _zipfile.extractall(path=_MANIFEST_FOLDER)
+        os.rename(filename, _CONTENT_FILE)
+        os.remove(_ZIP_TARGET)
 
 
 # SQLITE3 Setup
