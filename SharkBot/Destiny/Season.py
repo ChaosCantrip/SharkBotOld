e77335300d0e93b9315d252675ab2d5ca16a94d9
James Donald <dev@chaoscantrip.com>
2023-02-27 13:46:41 +0000

    Changed Season to read from manifest

--- SharkBot/Destiny/Season.py
+++ SharkBot/Destiny/Season.py
@@ -1,35 +1,84 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import json
+from typing import Any, Optional, Self
 
 from SharkBot import Utils
-
+from SharkBot.Destiny import Manifest
 
 class Season:
-    current = None
+    seasons: list[Self] = []
+    current: Optional[Self] = None
 
-    def __init__(self, name: str, number: int, start: str, end: str, icon: str) -> None:
-        self.name = name
-        self.number = number
-        self.start = datetime.strptime(start, "%d/%m/%Y")
-        self.start += timedelta(hours=18)
-        self.end = datetime.strptime(end, "%d/%m/%Y")
-        self.end += timedelta(hours=18)
-        self.icon = icon
+    def __init__(self, data: dict[str, Any]) -> None:
+        self._data = data
+        self.start: Optional[datetime] = None
+        self.end: Optional[datetime] = None
+        _start_date = data.get("startDate")
+        if _start_date is not None:
+            self.start = datetime.fromisoformat(_start_date)
+        _end_date = data.get("endDate")
+        if _end_date is not None:
+            self.end = datetime.fromisoformat(_end_date)
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def get(self, key, default=None):
+        return self._data.get(key, default)
 
     @property
-    def calendar_string(self) -> str:
-        return f"{datetime.strftime(self.start, '%d %b')} - {datetime.strftime(self.end, '%d %b')}"
+    def name(self) -> str:
+        return self._data["displayProperties"]["name"]
 
     @property
-    def time_remaining(self) -> timedelta:
-        return self.end - datetime.now()
+    def description(self) -> str:
+        return self._data["displayProperties"]["description"]
 
     @property
-    def time_remaining_string(self) -> str:
+    def number(self) -> int:
+        return self._data["seasonNumber"]
+
+    @property
+    def calendar_string(self) -> Optional[str]:
+        return f"{datetime.strftime(self.start, '%d %b') if self.start is not None else '???'} - {datetime.strftime(self.end, '%d %b') if self.end is not None else '???'}"
+
+    @property
+    def time_remaining(self) -> Optional[timedelta]:
+        if self.end is None:
+            return None
+        return self.end - datetime.utcnow()
+
+    @property
+    def time_remaining_string(self) -> Optional[str]:
+        if self.end is None:
+            return None
         return Utils.td_to_string(self.time_remaining)
 
+    @property
+    def has_season_pass(self) -> bool:
+        return self._data.get("seasonPassHash") is not None
 
-with open("data/static/destiny/current_season.json", "r") as infile:
-    data = json.load(infile)
+    @property
+    def season_pass_definition(self) -> Optional[dict]:
+        if self.has_season_pass:
+            return Manifest.get_definition("DestinySeasonPassDefinition", self._data["seasonPassHash"])
+        else:
+            return None
 
-Season.current = Season(**data)
+    @property
+    def progression_hashes(self) -> Optional[list[int]]:
+        season_pass = self.season_pass_definition
+        if season_pass is None:
+            return None
+        else:
+            return [season_pass["rewardProgressionHash"], season_pass["prestigeProgressionHash"]]
+
+Season.seasons = [Season(d) for d in Manifest.get_all_definitions("DestinySeasonDefinition").values()]
+now = datetime.utcnow().astimezone(timezone.utc)
+for season in Season.seasons:
+    if season.start is not None and season.end is not None:
+        if season.start < now < season.end:
+            Season.current = season
+            break
+
+Season.seasons.sort(key=lambda s: s.number)
