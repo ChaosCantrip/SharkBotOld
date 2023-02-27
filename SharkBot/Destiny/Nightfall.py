a8d8e5f10bdceac70da2e433cb94486c0c9cbd53
James Donald <dev@chaoscantrip.com>
2023-02-27 05:52:29 +0000

    Added type hints for NightfallDifficulty

--- SharkBot/Destiny/Nightfall.py
+++ SharkBot/Destiny/Nightfall.py
@@ -1,8 +1,8 @@
 class NightfallDifficulty:
 
     def __init__(self, activity_data: dict):
-        self.name = activity_data["description"]
-        self.difficulty = activity_data["name"]
+        self.name: str = activity_data["description"]
+        self.difficulty: str = activity_data["name"]
         self.light_level: int = activity_data["activityLightLevel"]
         self.destination_hash: int = activity_data["destinationHash"]
         self.modifier_hashes: list[int] = [m["activityModifierHash"] for m in activity_data["modifiers"]]
