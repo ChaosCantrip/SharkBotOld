45e78842f620f2db56da113b30ad968c30341d9b
James Donald <dev@chaoscantrip.com>
2023-02-27 14:53:55 +0000

    Added power bonus to $destiny season_levels

--- SharkBot/MemberBungie/BungieData/SeasonLevels.py
+++ SharkBot/MemberBungie/BungieData/SeasonLevels.py
@@ -10,13 +10,26 @@ class SeasonData:
     name: str
     number: int
     level: int
+    bonus: int
 
-    def __str__(self):
-        return f"`{self.number}` {self.name} `Level {self.level}`"
+def bonus_to_xp(bonus: int) -> int:
+    return ((bonus-2) * 110000) + 55000
+
+def xp_to_bonus(xp: int) -> int:
+    bonus = 0
+    total_xp = 0
+    while True:
+        total_xp += bonus_to_xp(bonus+1)
+        if total_xp > xp:
+            return bonus - 1
+        else:
+            bonus += 1
 
 class SeasonLevels(BungieData):
-    _COMPONENTS = [104, 202]
+    _COMPONENTS = [202]
     _THUMBNAIL_URL = None
+    _EMBED_TITLE = "Season Levels"
+    _EMBED_COLOUR = discord.Colour.dark_gold()
 
     @staticmethod
     def _process_data(data):
@@ -27,13 +40,15 @@ class SeasonLevels(BungieData):
             if season.has_season_pass:
                 if season.start > now:
                     continue
-                progress = 0
+                level = 0
+                xp = 0
                 for h in season.progression_hashes:
                     if str(h) not in character_progression:
                         continue
-                    progress += character_progression[str(h)]["level"]
-                if progress > 1:
-                    result.append(SeasonData(season.name, season.number, progress))
+                    level += character_progression[str(h)]["level"]
+                    xp += character_progression[str(h)]["currentProgress"]
+                if level > 1:
+                    result.append(SeasonData(season.name, season.number, level, xp_to_bonus(xp)))
         return result
 
     @staticmethod
@@ -52,7 +67,12 @@ class SeasonLevels(BungieData):
     def _format_embed_data(embed: discord.Embed, data: list[SeasonData], **kwargs):
         total_num = sum([s.level for s in data])
         embed.description = f"You have gained `{total_num} Levels` over your time in Destiny."
-        embed.add_field(
-            name="Season Levels",
-            value="\n".join(str(s) for s in data)
+        for s in data:
+            embed.add_field(
+                name=f"__Season {s.number}__",
+                value=f"**{s.name}**\nLevel: `{s.level}`\nPower Bonus: `+{s.bonus}`",
+                inline=False
+            )
+        embed.set_thumbnail(
+            url="https://bungie.net" + SharkBot.Destiny.Season.current.artifact_definition["displayProperties"]["icon"]
         )
