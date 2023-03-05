93311e40962967551ce31be6c63f64052ae4bec8
James Donald <dev@chaoscantrip.com>
2023-03-05 01:06:40 +0000

    Added first pass of data in PowerLevel

--- SharkBot/MemberBungie/BungieData/PowerLevel.py
+++ SharkBot/MemberBungie/BungieData/PowerLevel.py
@@ -27,7 +27,7 @@ class HashTranslations:
         49: "Class"
     }
 
-def _create_blank_dataset() -> dict[str, dict[str, int] | dict[str, dict[str, int]] | int]:
+def _create_blank_dataset():
     return {
         "Weapons": {
             "Kinetic": 0,
@@ -72,7 +72,43 @@ class PowerLevel(BungieData):
             for data in data[bucket_location]["data"].values():
                 item_buckets.append(data["items"])
 
-        return data
+        raw_data = _create_blank_dataset()
+        item_instances: dict[str, dict] = data["itemComponents"]["instances"]["data"]
+        for bucket in item_buckets:
+            for item in bucket:
+                if (item_instance_id:=item.get("itemInstanceId")) is None:
+                    continue
+                item_instance = item_instances[str(item_instance_id)]
+                if (primary_stat:=item_instance.get("primaryStat")) is None:
+                    continue
+                if (item_type:=HashTranslations.STAT_HASHES.get(primary_stat["statHash"])) is None:
+                    continue
+                stat_value: int = primary_stat["value"]
+                if item_type == "Power Bonus":
+                    raw_data["Power Bonus"] = max(stat_value, raw_data["Power Bonus"])
+                    continue
+                item_category_hashes: list[str] = SharkBot.Destiny.Definitions.DestinyInventoryItemDefinition.get(item["itemHash"])["itemCategoryHashes"]
+                if item_type == "Weapons":
+                    weapon_type = None
+                    for category_hash, category_name in HashTranslations.WEAPON_TYPES.items():
+                        if category_hash in item_category_hashes:
+                            weapon_type = category_name
+                            break
+                    if stat_value > raw_data["Weapons"][weapon_type]:
+                        raw_data["Weapons"][weapon_type] = stat_value
+                else: # Armour
+                    armor_class, armor_slot = None, None
+                    for category_hash, category_name in HashTranslations.ARMOUR_SLOTS.items():
+                        if category_hash in item_category_hashes:
+                            armor_slot = category_name
+                            break
+                    for category_hash, category_name in HashTranslations.ARMOUR_CLASSES.items():
+                        if category_hash in item_category_hashes:
+                            armor_class = category_name
+                            break
+                    if stat_value > raw_data["Armour"][armor_class][armor_slot]:
+                        raw_data["Armour"][armor_class][armor_slot] = stat_value
+        return raw_data
 
     # @staticmethod
     # def _process_cache_write(data):
