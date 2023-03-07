51af3d777deaad296c19e6ac4c71a39415696d0b
James Donald <dev@chaoscantrip.com>
2023-03-07 11:33:59 +0000

    Added state processing for Catalysts

--- SharkBot/MemberBungie/BungieData/Catalysts.py
+++ SharkBot/MemberBungie/BungieData/Catalysts.py
@@ -17,6 +17,7 @@ for _node in SUB_NODES:
         _records[str(_record_definition["hash"])] = _record_definition["displayProperties"]["name"]
     RECORD_NAMES[_node["displayProperties"]["name"]] = _records
 
+
 class Catalysts(BungieData):
     _COMPONENTS = [900]
     _THUMBNAIL_URL = None
@@ -28,9 +29,9 @@ class Catalysts(BungieData):
         ] + [
             _d["records"] for _d in data["characterRecords"]["data"].values()
         ]
-        _results: dict[str, dict[str, int]] = {}
+        _results: dict[str, dict[str, str]] = {}
         for _node_name, _node_data in RECORD_NAMES.items():
-            _node_records: dict[str, int] = {}
+            _node_records: dict[str, str] = {}
             for _record_hash, _record_name in _node_data.items():
                 _record_data = None
                 for _bucket in _record_buckets:
@@ -39,7 +40,16 @@ class Catalysts(BungieData):
                         break
                 else:
                     continue
-                _node_records[_record_name] = _record_data["state"]
+                _record_state = _record_data["state"]
+                if _record_state == 67 or _record_state == 0:
+                    continue
+                elif _record_state == 4:
+                    _completion_value = sum(o["completionValue"] for o in _record_data["objectives"])
+                    _progress = sum(o["progress"] if not o["complete"] else o["completionValue"] for o in _record_data["objectives"])
+                    _state = f"[{_progress}/{_completion_value}]"
+                else:
+                    _state = "[Not Found]"
+                _node_records[_record_name] = _state
             _results[_node_name] = _node_records
         return _results
 
