b4455ebede41fa3c6c93487ba7ad29e82149d15d
James Donald <dev@chaoscantrip.com>
2023-03-07 11:26:46 +0000

    Added basic state fetching of catalysts

--- SharkBot/MemberBungie/BungieData/Catalysts.py
+++ SharkBot/MemberBungie/BungieData/Catalysts.py
@@ -9,7 +9,7 @@ SUB_NODES = [
     for d in ROOT_NODE["children"]["presentationNodes"]
 ]
 
-RECORD_NAMES: dict[str, dict[int, str]] = {}
+RECORD_NAMES: dict[str, dict[str, str]] = {}
 for _node in SUB_NODES:
     _records = {}
     for d in _node["children"]["records"]:
@@ -21,9 +21,27 @@ class Catalysts(BungieData):
     _COMPONENTS = [900]
     _THUMBNAIL_URL = None
 
-    # @staticmethod
-    # def _process_data(data):
-    #     return data
+    @staticmethod
+    def _process_data(data):
+        _record_buckets: list[dict[str, dict]] = [
+             data["profileRecords"]["data"]["records"]
+        ] + [
+            _d["records"] for _d in data["characterRecords"]["data"].values()
+        ]
+        _results: dict[str, dict[str, int]] = {}
+        for _node_name, _node_data in RECORD_NAMES.items():
+            _node_records: dict[str, int] = {}
+            for _record_hash, _record_name in _node_data.items():
+                _record_data = None
+                for _bucket in _record_buckets:
+                    _record_data = _bucket.get(_record_hash)
+                    if _record_data is not None:
+                        break
+                else:
+                    continue
+                _node_records[_record_name] = _record_data["state"]
+            _results[_node_name] = _node_records
+        return _results
 
     # @staticmethod
     # def _process_cache_write(data):
