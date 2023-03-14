3c260c0a7149ff4bbcdfecf03700791da30e4ce1
James Donald <dev@chaoscantrip.com>
2023-03-14 23:27:46 +0000

    Changed PowerCap to IntEnum with values() classmethod inheriting from _Enum middle class

--- SharkBot/Destiny/PowerCap.py
+++ SharkBot/Destiny/PowerCap.py
@@ -1,15 +1,26 @@
-class Gear:
+from enum import IntEnum as _IntEnum
+
+class _Enum(_IntEnum):
+
+    @classmethod
+    def items(cls):
+        return [
+            (" ".join(name.split("_")).title(), int(value))
+            for name, value in cls.__members__.items()
+        ]
+
+class Gear(_Enum):
     POWER_FLOOR = 1600
     SOFT_CAP = 1750
     POWER_CAP = 1800
     HARD_CAP = POWER_CAP + 10
 
-class Activity:
+class Activity(_Enum):
     LEGEND = Gear.HARD_CAP + 20
     MASTER = Gear.HARD_CAP + 30
     GRANDMASTER = MASTER
 
-class MaxEffectivePower:
+class MaxEffectivePower(_Enum):
     LEGEND = Activity.LEGEND - 15
     MASTER = Activity.MASTER - 20
     GRANDMASTER = Activity.GRANDMASTER - 25
