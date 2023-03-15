92405311ed898838f3245620a055cb94928b107c
James Donald <dev@chaoscantrip.com>
2023-03-15 11:54:14 +0000

    Added BungieData.Pinnacles from template

---
+++ SharkBot/MemberBungie/BungieData/Pinnacles.py
@@ -0,0 +1,29 @@
+import discord
+
+from .BungieData import BungieData
+import SharkBot
+
+
+class Pinnacles(BungieData):
+    _COMPONENTS = []
+    _THUMBNAIL_URL = None
+
+    # @staticmethod
+    # def _process_data(data):
+    #     return data
+
+    # @staticmethod
+    # def _process_cache_write(data):
+    #     return data
+
+    # @staticmethod
+    # def _process_cache_load(data):
+    #     return data
+
+    # @classmethod
+    # def _format_cache_embed_data(cls, embed: discord.Embed, data, **kwargs):
+    #     cls._format_embed_data(embed, data)
+
+    # @staticmethod
+    # def _format_embed_data(embed: discord.Embed, data, **kwargs):
+    #     embed.description = f"\n```{SharkBot.Utils.JSON.dumps(data)}```"
