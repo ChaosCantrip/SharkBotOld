0b9ec1c917990ce77427316800f79b92d6303aae
James Donald <dev@chaoscantrip.com>
2023-03-25 02:55:08 +0000

    Added initial construction of GuardianRank definitions

--- SharkBot/MemberBungie/BungieData/GuardianRanks.py
+++ SharkBot/MemberBungie/BungieData/GuardianRanks.py
@@ -3,9 +3,47 @@ import discord
 from .BungieData import BungieData
 import SharkBot
 
+root_node = SharkBot.Destiny.Definitions.DestinyPresentationNodeDefinition.get("3741753466")
+
+child_nodes = [SharkBot.Destiny.Definitions.DestinyPresentationNodeDefinition.get(d["presentationNodeHash"]) for d in root_node["children"]["presentationNodes"]]
+
+
+class Record:
+
+    def __init__(self, definition: dict):
+        self.name = definition["displayProperties"]["name"]
+        self.description = definition["displayProperties"]["description"]
+        self.completion_value: int = SharkBot.Destiny.Definitions.DestinyObjectiveDefinition.get(definition["objectiveHashes"][0])["completionValue"]
+
+    def __repr__(self):
+        return f"Record[\n  {self.name}\n  {self.description}\n  {self.completion_value}\n]"
+
+class RecordSet:
+
+    def __init__(self, definition: dict):
+        self.name = definition["displayProperties"]["name"]
+        self.description = definition["displayProperties"]["description"]
+        self.records = [Record(SharkBot.Destiny.Definitions.DestinyRecordDefinition.get(d["recordHash"])) for d in definition["children"]["records"]]
+
+    def __repr__(self):
+        return f"RecordSet[\n  {self.name}\n  {self.description}\n  {self.records}\n]"
+
+class GuardianRank:
+
+    def __init__(self, number: int, definition: dict):
+        self.number = number
+        self.name = definition["displayProperties"]["name"]
+        self.description = definition["displayProperties"]["description"]
+        self.record_sets = [RecordSet(SharkBot.Destiny.Definitions.DestinyPresentationNodeDefinition.get(d["presentationNodeHash"])) for d in definition["children"]["presentationNodes"]]
+        self.completion_record_hash = definition["completionRecordHash"]
+
+    def __repr__(self):
+        return f"GuardianRank[\n  {self.number}\n  {self.name}\n  {self.description}\n  {self.record_sets}\n]"
+
+GUARDIAN_RANKS = [GuardianRank(i + 1, node) for i, node in enumerate(child_nodes)]
 
 class GuardianRanks(BungieData):
-    _COMPONENTS = []
+    _COMPONENTS = [104,1200]
     _THUMBNAIL_URL = None
 
     # @staticmethod
