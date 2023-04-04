7a5d5ef286e6c0a26fb7dd29e81ea17356cab4c5
James Donald <dev@chaoscantrip.com>
2023-04-04 17:28:05 +0100

    Finalised Monument output

--- SharkBot/MemberBungie/BungieData/Monument.py
+++ SharkBot/MemberBungie/BungieData/Monument.py
@@ -49,6 +49,15 @@ class CollectibleState:
         self.UNIQUENESS_VIOLATION = bool(state_num & 0b100000)
         self.PURCHASE_DISABLED = bool(state_num & 0b1000000)
 
+    @property
+    def flags(self) -> list[str]:
+        _flags = []
+        if self.CANNOT_AFFORD_MATERIALS:
+            _flags.append(":green_square:")
+        else:
+            _flags.append(":red_square:")
+        return _flags
+
 
 _MONUMENT_DEFINITION = SharkBot.Destiny.Definitions.DestinyVendorDefinition.get(4230408743)
 _VENDOR_ITEMS = [ItemCategory(item["itemHash"]) for item in _MONUMENT_DEFINITION["itemList"] if item["itemHash"] not in _ignored_hashes]
@@ -56,7 +65,9 @@ _VENDOR_ITEMS = [ItemCategory(item["itemHash"]) for item in _MONUMENT_DEFINITION
 
 class Monument(BungieData):
     _COMPONENTS = [SharkBot.Destiny.Enums.ComponentType.Collectibles.value]
-    _THUMBNAIL_URL = None
+    _THUMBNAIL_URL = f"https://www.bungie.net{_MONUMENT_DEFINITION['displayProperties']['icon']}"
+    _EMBED_TITLE = "Monument of Lost Lights"
+    _EMBED_COLOUR = discord.Colour.gold()
 
     @staticmethod
     def _process_data(data) -> dict[str, dict[str, CollectibleState]]:
@@ -90,6 +101,27 @@ class Monument(BungieData):
     # def _format_cache_embed_data(cls, embed: discord.Embed, data, **kwargs):
     #     cls._format_embed_data(embed, data)
 
-    # @staticmethod
-    # def _format_embed_data(embed: discord.Embed, data, **kwargs):
-    #     embed.description = f"\n```{SharkBot.Utils.JSON.dumps(data)}```"
+    @staticmethod
+    def _format_embed_data(embed: discord.Embed, data: dict[str, dict[str, CollectibleState]], **kwargs):
+        num_items: int = 0
+        for category_name, category_data in data.items():
+            category_items: dict[str, CollectibleState] = {
+                item_name: item_state for item_name, item_state in category_data.items() if item_state.NOT_ACQUIRED
+            }
+            num_items += len(category_items)
+            if category_items:
+                embed.add_field(
+                    name=f"__{category_name}__",
+                    value="\n".join([
+                        f"{item_state.flags[0]} {item_name}" for item_name, item_state in category_items.items()
+                    ]),
+                    inline=False
+                )
+        if len(embed.fields) == 0:
+            embed.add_field(name="No items left to collect!", value=":tada:", inline=False)
+        embed.description = "\n".join([
+            f":green_square: - Materials Owned",
+            f":red_square: - Materials Needed",
+            f"\n**{num_items}** items left to collect!\n"
+        ])
+
