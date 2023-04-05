4b86d2bf89bfc88b1c7e51f76a04644e50db83c0
James Donald <dev@chaoscantrip.com>
2023-04-05 18:29:09 +0100

    Added ask_sharkbot()

--- SharkBot/Handlers/openaiHandler.py
+++ SharkBot/Handlers/openaiHandler.py
@@ -1,18 +1,31 @@
+from typing import TypedDict
 import aiohttp
 import secret
 
 
-async def make_request(messages: list[str]) -> tuple[int, dict]:
+class Prompt(TypedDict):
+    role: str
+    content: str
+
+
+SHARKBOT_PROMPT = Prompt(
+    role="system",
+    content="You are SharkBot, an AI Bot that is designed to give fun and interesting responses. You can use discord markdown and emojis to make your responses look better."
+)
+
+
+async def make_request(messages: list[dict]) -> tuple[int, dict]:
     async with aiohttp.ClientSession() as session:
         async with session.post(
                 "https://api.openai.com/v1/chat/completions",
                 headers=secret.OpenAI.API_HEADERS,
                 json={
                     "model": "gpt-3.5-turbo",
-                    "messages": [{
-                        "role": "user",
-                        "content": message
-                    } for message in messages],
+                    "messages": messages
                 }
         ) as resp:
             return resp.status, await resp.json()
+
+
+async def ask_sharkbot(message: str) -> tuple[int, dict]:
+    return await make_request([SHARKBOT_PROMPT, {"role": "user", "content": message}])
