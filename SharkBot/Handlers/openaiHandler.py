579bf39c4aabcd0b5b9df046746de1f291ebdf0a
James Donald <dev@chaoscantrip.com>
2023-04-05 17:50:54 +0100

    Added openaiHandler.py

---
+++ SharkBot/Handlers/openaiHandler.py
@@ -0,0 +1,18 @@
+import aiohttp
+import secret
+
+
+async def make_request(messages: list[str]):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                "https://api.openai.com/v1/chat/completions",
+                headers=secret.OpenAI.API_HEADERS,
+                json={
+                    "model": "gtp-3.5-turbo",
+                    "messages": [{
+                        "role": "user",
+                        "content": message
+                    } for message in messages],
+                }
+        ) as resp:
+            return await resp.json()
