12222c560ed20d0aae9ce640809bfef1b713f6f7
James Donald <dev@chaoscantrip.com>
2023-04-05 19:10:56 +0100

    Changed to ask_sharkbot_wrapper

--- cogs/openai.py
+++ cogs/openai.py
@@ -16,33 +16,37 @@ class OpenAI(commands.Cog):
 
     @commands.command()
     async def ask(self, ctx: commands.Context, *, prompt: str):
+        await self.ask_sharkbot_wrapper(ctx.message, prompt)
+
+    async def ask_sharkbot_wrapper(self, message: discord.Message, prompt: str):
         if len(prompt) > 200:
             raise SharkBot.Errors.OpenAI.PromptTooLongError(prompt)
-        if ctx.author.id in _usage_dict:
-            if ctx.author.id == self.bot.owner_id:
+        if message.author.id in _usage_dict:
+            if message.author.id == self.bot.owner_id:
                 time_diff = None
-            elif ctx.author.id in SharkBot.IDs.mods:
+            elif message.author.id in SharkBot.IDs.mods:
                 time_diff = timedelta(minutes=5)
             else:
                 time_diff = timedelta(hours=1)
-            if time_diff is not None and datetime.now() - _usage_dict[ctx.author.id] < time_diff:
+            if time_diff is not None and datetime.now() - _usage_dict[message.author.id] < time_diff:
                 raise SharkBot.Errors.OpenAI.TooManyRequestsError(time_diff)
         embed = discord.Embed()
         embed.title = prompt
         embed.description = "Thinking..."
         embed.set_author(
-            name=ctx.author.display_name + " asked...",
-            icon_url=ctx.author.display_avatar.url
+            name=message.author.display_name + " asked...",
+            icon_url=message.author.display_avatar.url
         )
         embed.set_thumbnail(
             url="https://miro.medium.com/v2/resize:fit:400/1*mMTHT8fwuCvSSp0RNXM4nA.gif"
         )
-        message = await ctx.reply(embed=embed, mention_author=False)
+        message = await message.reply(embed=embed, mention_author=False)
         response = await SharkBot.Handlers.openaiHandler.ask_sharkbot(prompt)
         embed.description = response[1]["choices"][0]["message"]["content"]
         embed.set_thumbnail(url=self.bot.user.display_avatar.url)
         await message.edit(embed=embed)
-        _usage_dict[ctx.author.id] = datetime.now()
+        _usage_dict[message.author.id] = datetime.now()
+
 
 
 async def setup(bot):
