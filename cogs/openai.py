587b8bf7c48ad7f1c12e08f52b957dc137778512
James Donald <dev@chaoscantrip.com>
2023-04-05 18:32:04 +0100

    Added $ask command

--- cogs/openai.py
+++ cogs/openai.py
@@ -1,11 +1,21 @@
 import discord
 from discord.ext import tasks, commands
 
+import SharkBot
+
 
 class OpenAI(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def ask(self, ctx: commands.Context, *, prompt: str):
+        message = await ctx.reply("Working on it...", mention_author=False)
+        response = await SharkBot.Handlers.openaiHandler.ask_sharkbot(prompt)
+        await message.edit(content=response[1]["choices"][0]["message"]["content"])
+
 
 
 async def setup(bot):
