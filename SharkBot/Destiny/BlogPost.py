038a5ebf72db522f7628a7ac69e9765fc63e02cf
James Donald <dev@chaoscantrip.com>
2023-04-06 03:16:50 +0100

    Added BlogPost constructor

--- SharkBot/Destiny/BlogPost.py
+++ SharkBot/Destiny/BlogPost.py
@@ -1,2 +1,17 @@
+import aiohttp
+from datetime import datetime
+
+from SharkBot import Utils
+import secret
+
+
 class BlogPost:
-    pass
+
+    def __init__(self, response_data: dict[str, str]):
+        self.title = response_data["Title"]
+        self.link = f"https://www.bungie.net{response_data['Link']}"
+        self.publish_date = datetime.fromisoformat(response_data["PubDate"])
+        self.id = response_data["UniqueIdentifier"]
+        self.description = response_data["Description"]
+        self.image = response_data.get("ImagePath")
+        self.mobile_image = response_data.get("OptionalMobileImagePath")
