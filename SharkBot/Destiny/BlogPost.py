9fa5835a659d7e852107c952111cbba00e70eedc
James Donald <dev@chaoscantrip.com>
2023-04-06 03:21:56 +0100

    Changed BlogPost constructor to use get()

--- SharkBot/Destiny/BlogPost.py
+++ SharkBot/Destiny/BlogPost.py
@@ -10,11 +10,11 @@ import secret
 class BlogPost:
 
     def __init__(self, response_data: dict[str, str]):
-        self.title = response_data["Title"]
-        self.link = f"https://www.bungie.net{response_data['Link']}"
+        self.title = response_data.get("Title")
+        self.link = f"https://www.bungie.net{response_data.get('Link')}"
         self.publish_date = datetime.fromisoformat(response_data["PubDate"])
-        self.id = response_data["UniqueIdentifier"]
-        self.description = response_data["Description"]
+        self.id = response_data.get("UniqueIdentifier")
+        self.description = response_data.get("Description")
         self.image = response_data.get("ImagePath")
         self.mobile_image = response_data.get("OptionalMobileImagePath")
 
