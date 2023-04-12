eb867124e1d0827081ecf30df9120626e74abdf8
James Donald <dev@chaoscantrip.com>
2023-04-13 00:47:09 +0100

    Added ProfileResponseData.items property

--- SharkBot/MemberBungie/ProfileResponseData.py
+++ SharkBot/MemberBungie/ProfileResponseData.py
@@ -1,3 +1,6 @@
+from typing import Iterator
+
+
 class ProfileResponseData:
 
     def __init__(self, data: dict):
@@ -5,3 +8,12 @@ class ProfileResponseData:
 
     def __getattr__(self, item):
         return self.data[item]
+
+    @property
+    def items(self) -> Iterator[dict]:
+        for item_data in self.data["profileInventory"]["data"]["items"]:
+            yield item_data
+        for bucket in ["characterInventories", "characterEquipment"]:
+            for character in self.data[bucket]["data"].values():
+                for item_data in character["items"]:
+                    yield item_data
