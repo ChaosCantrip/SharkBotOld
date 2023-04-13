60042c735647374b76213d3977dac45f43d75237
James Donald <dev@chaoscantrip.com>
2023-04-13 01:24:20 +0100

    Changed Conqueror to pull from manifest

--- SharkBot/MemberBungie/BungieData/Conqueror.py
+++ SharkBot/MemberBungie/BungieData/Conqueror.py
@@ -3,8 +3,13 @@ import discord
 from .BungieData import BungieData
 from ..ProfileResponseData import ProfileResponseData
 import SharkBot
+from SharkBot.Destiny import Definitions
 
-conqueror_definitions: dict[str, str] = SharkBot.Utils.JSON.load("data/static/bungie/definitions/Conqueror.json")
+cutoff = len("Grandmaster: ")
+conqueror_node = Definitions.DestinyPresentationNodeDefinition.get("3776992251")
+record_definitions = [
+    Definitions.DestinyRecordDefinition.get(record["recordHash"]) for record in conqueror_node["children"]["records"]
+]
 
 
 class Conqueror(BungieData):
@@ -14,12 +19,20 @@ class Conqueror(BungieData):
     @staticmethod
     def _process_data(data: ProfileResponseData):
         records = data["profileRecords"]["data"]["records"]
-        result: dict[str, bool] = {}
-        for record_hash, nightfall_name in conqueror_definitions.items():
-            record_data = records[record_hash]
-            complete = all([objective["complete"] for objective in record_data["objectives"]])
-            result[nightfall_name] = complete
-        return result
+        results: list[dict[str, str | bool]] = []
+        for record_definition in record_definitions:
+            record_data = records[str(record_definition["hash"])]
+            try:
+                complete = all([objective["complete"] for objective in record_data["objectives"]])
+            except KeyError:
+                complete = all([objective["complete"] for objective in record_data["intervalObjectives"]])
+            results.append({
+                "name": record_definition["displayProperties"]["name"],
+                "description": record_definition["displayProperties"]["description"],
+                "forTitleGilding": record_definition["forTitleGilding"],
+                "complete": complete
+            })
+        return results
 
     # @staticmethod
     # def _process_cache_write(data):
@@ -34,19 +47,29 @@ class Conqueror(BungieData):
     #     cls._format_embed_data(embed, data)
 
     @staticmethod
-    def _format_embed_data(embed: discord.Embed, data: dict[str, bool], **kwargs):
-        incomplete: list[str] = []
-        for nightfall, complete in data.items():
-            if not complete:
-                incomplete.append(nightfall)
-        if len(incomplete) > 0:
-            embed.description = f"You have `{len(incomplete)}` Grandmasters left to complete."
-            for nightfall_name in incomplete:
-                nightfall = SharkBot.Destiny.Nightfall.get(nightfall_name)
+    def _format_embed_data(embed: discord.Embed, data: list[dict[str, str | bool]], **kwargs):
+        title_gilded = all([record["complete"] for record in data if not record["forTitleGilding"]])
+        incomplete: list[dict[str, str | bool]] = [
+            record for record in data if record["forTitleGilding"] == title_gilded and not record["complete"]
+        ]
+        if len(incomplete) == 0:
+            embed.description = f"You have completed Conqueror this season!"
+            return
+        if title_gilded:
+            embed.description = f"You have {len(incomplete)} GMs to complete this season!"
+            for record in incomplete:
+                nightfall_name = record["name"][cutoff:]
+                nightfall = SharkBot.Destiny.Nightfall.get(nightfall_name).grandmaster
                 embed.add_field(
                     name=nightfall_name,
-                    value=nightfall.grandmaster.icons_str,
+                    value=nightfall.icons_str,
                     inline=False
                 )
         else:
-            embed.description = f"You have completed Conqueror this season!"
+            embed.description = f"You have {len(incomplete)} Objectives left to complete!"
+            for record in incomplete:
+                embed.add_field(
+                    name=record["name"],
+                    value=record["description"],
+                    inline=False
+                )
