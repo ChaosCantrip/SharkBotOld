b5908f658d67d2e01e0b8de26a1f11d42346e2f9
ChaosCantrip <dev@chaoscantrip.com>
2023-07-11 05:37:15 +0100

    Added Warframe.WorldState Landscape Cycles

--- SharkBot/Warframe/WorldState.py
+++ SharkBot/Warframe/WorldState.py
@@ -1,9 +1,35 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Literal, Self
 
 import aiohttp
 
 import SharkBot
+
+
+class LandscapeCycle:
+
+    def __init__(self, data: dict):
+        self._data = data
+
+    @property
+    def id(self) -> str:
+        return self._data["id"]
+
+    @property
+    def activation(self) -> datetime:
+        return datetime.fromisoformat(self._data["activation"])
+
+    @property
+    def expiry(self) -> datetime:
+        return datetime.fromisoformat(self._data["expiry"])
+
+    @property
+    def expires_in(self) -> timedelta:
+        return self.expiry - datetime.utcnow()
+
+    @property
+    def state(self) -> str:
+        return self._data["state"]
 
 
 class WorldState:
@@ -20,4 +46,25 @@ class WorldState:
                     return cls(await response.json())
                 else:
                     raise SharkBot.Errors.WarframeAPI.InternalServerError(response, await response.text())
+
+
+    @property
+    def timestamp(self) -> datetime:
+        return self._timestamp
+
+    @property
+    def earth_cycle(self) -> LandscapeCycle:
+        return LandscapeCycle(self._data["earthCycle"])
+
+    @property
+    def cetus_cycle(self) -> LandscapeCycle:
+        return LandscapeCycle(self._data["cetusCycle"])
+
+    @property
+    def vallis_cycle(self) -> LandscapeCycle:
+        return LandscapeCycle(self._data["vallisCycle"])
+
+    @property
+    def cambion_cycle(self) -> LandscapeCycle:
+        return LandscapeCycle(self._data["cambionCycle"])
 
