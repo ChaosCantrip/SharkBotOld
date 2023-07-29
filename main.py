4d4c691800b85d542df395743aa99aa92fb537af
ChaosCantrip <dev@chaoscantrip.com>
2023-07-29 23:49:23 +0100

    Changed logging to external import for code readability

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-import json
+import SharkBotLogging
 from datetime import datetime, timedelta
 import logging
 import logging.handlers
 import os
 import secret
-
-if not os.path.isdir("data/live/bot/logs"):
-    os.makedirs("data/live/bot/logs")
-
-LOG_FORMAT_DICT = {
-    "source": "SharkBot",
-    "time": "%(asctime)s",
-    "level": "%(levelname)s",
-    "logger": "%(name)s",
-    "message": "%(message)s"
-}
-
-
-CURRENT_LOGFILE = f"data/live/bot/logs/{int(datetime.utcnow().timestamp())}.log"
-
-logging.basicConfig(
-    filename=CURRENT_LOGFILE,
-    filemode="w",
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
-)
-
-logging.info("SharkBot Initialising")
-
 import asyncio
 import sys
 import discord
